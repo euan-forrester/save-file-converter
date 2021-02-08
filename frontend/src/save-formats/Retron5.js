bda077474880e138e9a3af4006e48f33dcf7f306
Euan <euan_forrester@hotmail.com>
2021-02-08 14:30:37 -0800

    Successfully parsed retron5 save data

--- frontend/src/save-formats/Retron5.js
+++ frontend/src/save-formats/Retron5.js
@@ -1,33 +1,57 @@
+/*
+The Retron5 data format is:
+
+typedef struct
+{
+   uint32_t magic;
+   uint16_t fmtVer;
+   uint16_t flags;
+   uint32_t origSize;
+   uint32_t packed_size;
+   uint32_t data_offset;
+   uint32_t crc32;
+   uint8_t data[0];
+} t_retronDataHdr;
+*/
+
+const LITTLE_ENDIAN = true;
+
 export default class Retron5SaveData {
   constructor(blob) {
+    this.dataView = new DataView(blob);
     this.byteLength = blob.byteLength;
   }
 
   getMagic() {
-    return this.byteLength;
+    return this.dataView.getUint32(0, LITTLE_ENDIAN);
   }
 
   getFormatVersion() {
-    return this.byteLength;
+    return this.dataView.getUint16(4, LITTLE_ENDIAN);
   }
 
   getFlags() {
-    return this.byteLength;
+    return this.dataView.getUint16(6, LITTLE_ENDIAN);
   }
 
   getOriginalSize() {
-    return this.byteLength;
+    return this.dataView.getUint32(8, LITTLE_ENDIAN);
   }
 
   getPackedSize() {
-    return this.byteLength;
+    return this.dataView.getUint32(12, LITTLE_ENDIAN);
   }
 
   getDataOffset() {
-    return this.byteLength;
+    return this.dataView.getUint32(16, LITTLE_ENDIAN);
   }
 
   getCrc32() {
+    return this.dataView.getUint32(20, LITTLE_ENDIAN);
+  }
+
+  getRawSaveData() {
+    // FIXME: How to get ArrayBuffer starting at an offset from the ArrayBuffer we were passed?
     return this.byteLength;
   }
 }
