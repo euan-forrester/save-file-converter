a0f7ff5c4ed2cc9a9c3e58433773df49110f8d07
Euan <euan_forrester@hotmail.com>
2021-02-22 16:37:49 -0800

    Less bad About page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Save File Converter</h1>
+    <div>
+      Convert save files for retro consoles between different formats
+    </div>
+    <br/>
+    <div>
+      Questions/comments/suggestions? savefileconverter (at) gmail.com
+    </div>
   </div>
 </template>
