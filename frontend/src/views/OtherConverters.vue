b08e680c33abb931e37ab2729f5cbc8fcd89256f
Euan <euan_forrester@hotmail.com>
2021-04-10 11:41:47 -0700

    Fixed Other Converters layout on mobile

--- frontend/src/views/OtherConverters.vue
+++ frontend/src/views/OtherConverters.vue
@@ -7,31 +7,31 @@
         </h2>
       </b-col>
     </b-row>
+    <div class="blank-line"/>
     <div class="d-none d-md-block">
-      <b-row>
-        <b-col>
+      <b-row align-h="center">
+        <b-col cols=5 lg=4 xl=3>
           <h5>Link</h5>
         </b-col>
-        <b-col>
+        <b-col cols=5 lg=4 xl=3>
           <h5>Notes</h5>
         </b-col>
       </b-row>
     </div>
-    <div class="blank-line d-md-none"/>
-    <b-row>
-      <b-col cols=12 md=6>
+    <b-row align-h="center">
+      <b-col cols=12 md=5 lg=4 xl=3>
         <b-link href="http://www.save-editor.com/tools/">http://www.save-editor.com/tools/</b-link>
       </b-col>
-      <b-col cols=12 md=6>
+      <b-col cols=12 md=5 lg=4 xl=3>
         Lots of different tools available here
       </b-col>
     </b-row>
     <div class="blank-line d-md-none"/>
-    <b-row>
-      <b-col cols=12 md=6>
+    <b-row align-h="center">
+      <b-col cols=12 md=5 lg=4 xl=3>
         <b-link href="https://shunyweb.info/convert.php">https://shunyweb.info/convert.php</b-link>
       </b-col>
-      <b-col cols=12 md=6>
+      <b-col cols=12 md=5 lg=4 xl=3>
         Mostly Nintendo DS save formats
       </b-col>
     </b-row>
