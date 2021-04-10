c0664ea4395a4f236d82059da48cb83f9ac63c66
Euan <euan_forrester@hotmail.com>
2021-04-10 11:30:07 -0700

    Remove unneeded tag in jumbotrons

--- frontend/src/views/OtherConverters.vue
+++ frontend/src/views/OtherConverters.vue
@@ -7,29 +7,39 @@
         </h2>
       </b-col>
     </b-row>
+    <div class="d-none d-md-block">
+      <b-row>
+        <b-col>
+          <h5>Link</h5>
+        </b-col>
+        <b-col>
+          <h5>Notes</h5>
+        </b-col>
+      </b-row>
+    </div>
+    <div class="blank-line d-md-none"/>
     <b-row>
-      <b-col>
-        <h5>Link</h5>
-      </b-col>
-      <b-col>
-        <h5>Notes</h5>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
+      <b-col cols=12 md=6>
         <b-link href="http://www.save-editor.com/tools/">http://www.save-editor.com/tools/</b-link>
       </b-col>
-      <b-col>
+      <b-col cols=12 md=6>
         Lots of different tools available here
       </b-col>
     </b-row>
+    <div class="blank-line d-md-none"/>
     <b-row>
-      <b-col>
+      <b-col cols=12 md=6>
         <b-link href="https://shunyweb.info/convert.php">https://shunyweb.info/convert.php</b-link>
       </b-col>
-      <b-col>
+      <b-col cols=12 md=6>
         Mostly Nintendo DS save formats
       </b-col>
     </b-row>
   </div>
 </template>
+
+<style scoped>
+.blank-line {
+  min-height: 1em;
+}
+</style>
