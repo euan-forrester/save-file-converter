0ec2a0955b48b146c0f64c73cd6ea3beeb64935a
Euan <euan_forrester@hotmail.com>
2021-05-20 12:22:19 -0700

    Put upper tabs in 2 rows for readability and to prep for adding Troubleshooting tab

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/retron-5">Retron 5</router-link> |
-      <router-link to="/gameshark">GameShark (GBA)</router-link> |
-      <router-link to="/gameshark-sp">GameShark SP (GBA)</router-link> |
-      <router-link to="/action-replay">Action Replay (GBA)</router-link> |
-      <router-link to="/other-converters">Other converters</router-link> |
-      <router-link to="/about">About/Contact</router-link>
+      <b-row>
+        <b-col class="nav-row">
+          <router-link to="/retron-5">Retron 5</router-link> |
+          <router-link to="/gameshark">GameShark (GBA)</router-link> |
+          <router-link to="/gameshark-sp">GameShark SP (GBA)</router-link> |
+          <router-link to="/action-replay">Action Replay (GBA)</router-link>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <router-link to="/other-converters">Other converters</router-link> |
+          <router-link to="/about">About/Contact</router-link>
+        </b-col>
+      </b-row>
     </div>
     <router-view/>
     <mq-layout :mq="['xs', 'sm']">
@@ -35,6 +43,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.nav-row {
+  padding-bottom: 1em;
 }
 
 .footer {
