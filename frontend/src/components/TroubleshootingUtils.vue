494b6e4d8f1d5962467d9e3025bee4be77aaacb9
Euan <euan_forrester@hotmail.com>
2021-05-26 11:53:47 -0700

    Layout update for new comparison method of troubleshooting

--- frontend/src/components/TroubleshootingUtils.vue
+++ frontend/src/components/TroubleshootingUtils.vue
@@ -6,31 +6,64 @@
           <b-row no-gutters align-h="center" align-v="start">
             <b-col cols=12>
               <b-jumbotron fluid :header-level="$mq | mq({ xs: 5, sm: 5, md: 5, lg: 5, xl: 4 })">
-                <template v-slot:header>Emulator/Raw</template>
+                <template v-slot:header>Emulator/Raw<br>Test save</template>
               </b-jumbotron>
             </b-col>
           </b-row>
           <input-file
-            @load="readRawData($event)"
+            @load="readRetron5SaveData($event)"
             :errorMessage="this.errorMessage"
-            placeholderText="Choose a file to troubleshoot (*.srm)"
+            placeholderText="Choose a file that you know works (*.srm)"
             acceptExtension=".srm"
-            :leaveRoomForHelpIcon="false"
+            :leaveRoomForHelpIcon="true"
+            helpText="Make a test save using the emulator or cartridge that you want to use, to compare against the file that's not working."
+            id="known-working-input-file"
           />
+        </b-col>
+        <b-col sm=12 md=2 lg=2 xl=2 align-self="start">
+          <mq-layout :mq="['md', 'lg', 'xl']">
+            <i class="fa fa-arrow-circle-right fa-3x horizontal-arrow"></i>
+          </mq-layout>
+          <mq-layout :mq="['xs', 'sm']">
+            <i class="fa fa-arrow-circle-down fa-3x vertical-arrow"></i>
+          </mq-layout>
         </b-col>
         <b-col sm=12 md=5 align-self="start">
           <b-row no-gutters align-h="center" align-v="start">
-
+            <b-col cols=12>
+              <b-jumbotron fluid :header-level="$mq | mq({ xs: 5, sm: 5, md: 5, lg: 5, xl: 4 })">
+                <template v-slot:header>Emulator/Raw Needs fixed</template>
+              </b-jumbotron>
+            </b-col>
           </b-row>
+          <input-file
+            @load="readEmulatorSaveData($event)"
+            :errorMessage="this.errorMessage"
+            placeholderText="Choose the file that's not working (*.srm)"
+            acceptExtension=".srm"
+            :leaveRoomForHelpIcon="true"
+            helpText="The system will compare the file that's not working against the test save you made to try and figure out how to change it."
+            id="needs-fixed-input-file"
+          />
+        </b-col>
+      </b-row>
+      <b-row class="justify-content-md-center" align-h="center">
+        <b-col cols="auto" sm=4 md=3 lg=2 align-self="center">
+          <b-button
+            class="troubleshooting-button"
+            variant="success"
+            block
+            :disabled="!this.retron5SaveData || !outputFilename"
+            @click="convertFile()"
+          >
+          Attempt fix!
+          </b-button>
         </b-col>
       </b-row>
       <b-row>
         <b-col>
           <div class="help">
-            Tip 1: Create a test save. Compare its size with the file you're troubleshooting.
-          </div>
-          <div class="help">
-            Tip 2: Use a hex editor to open the test save and the one you're troubleshooting. Look for any similarities or differences.
+            Tip: if this tool doesn't work for you, try opening both files in a hex editor and looking for similarities or differences that may help you fix the file.
           </div>
           <div class="help">
             Did this page help you? Please tell me if it did or if it didn't: savefileconverter{{'\xa0'}}(at){{'\xa0'}}gmail{{'\xa0'}}(dot){{'\xa0'}}com
@@ -44,13 +77,25 @@
 <style scoped>
 
 /* Separate class for each different button to enable tracking in google tag manager */
-.retron5-convert-button {
+.troubleshooting-button {
   margin-top: 1em;
 }
 
 .help {
   margin-top: 1em;
 }
+
+.horizontal-arrow {
+  margin-top: 2em;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+}
+
+.vertical-arrow {
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+}
+
 </style>
 
 <script>
