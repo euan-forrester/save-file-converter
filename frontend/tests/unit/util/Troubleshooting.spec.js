19c7b4cb371379c240b797238247d702fa94f06d
Euan <euan_forrester@hotmail.com>
2021-05-30 11:01:39 -0700

    Added more tests to Troubleshooting module

--- frontend/tests/unit/util/Troubleshooting.spec.js
+++ frontend/tests/unit/util/Troubleshooting.spec.js
@@ -10,6 +10,197 @@ const PADDING_VALUE_B = 0xFF;
 const EXTRA_PADDING_COUNT = 16;
 
 describe('Troubleshooting', () => {
+  it('should get the pad value and count', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      if (i < EXTRA_PADDING_COUNT) {
+        a[i] = PADDING_VALUE_A;
+      } else {
+        a[i] = DATA_VALUE;
+      }
+    });
+
+    const padding = Troubleshooting.getPadValueAndCount(arrayBuffer);
+
+    expect(padding.value).to.equal(PADDING_VALUE_A);
+    expect(padding.count).to.equal(EXTRA_PADDING_COUNT);
+  });
+
+  it('should get the other pad value and count', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      if (i < EXTRA_PADDING_COUNT) {
+        a[i] = PADDING_VALUE_B;
+      } else {
+        a[i] = DATA_VALUE;
+      }
+    });
+
+    const padding = Troubleshooting.getPadValueAndCount(arrayBuffer);
+
+    expect(padding.value).to.equal(PADDING_VALUE_B);
+    expect(padding.count).to.equal(EXTRA_PADDING_COUNT);
+  });
+
+  it('should maintain data size being a power of 2 when finding padding', () => {
+    expect(Troubleshooting.getNextLargestPowerOf2(ARRAY_BUFFER_SIZE)).to.equal(ARRAY_BUFFER_SIZE);
+
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      if (i < (EXTRA_PADDING_COUNT + 1)) { // One extra byte of padding
+        a[i] = PADDING_VALUE_B;
+      } else {
+        a[i] = DATA_VALUE;
+      }
+    });
+
+    const padding = Troubleshooting.getPadValueAndCount(arrayBuffer);
+
+    expect(padding.value).to.equal(PADDING_VALUE_B);
+    expect(padding.count).to.equal(EXTRA_PADDING_COUNT); // The extra byte of padding isn't counted
+  });
+
+  it('should get the pad count when there\'s no padding', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      a[i] = DATA_VALUE;
+    });
+
+    const padding = Troubleshooting.getPadValueAndCount(arrayBuffer);
+
+    expect(padding.count).to.equal(0);
+  });
+
+  it('should get the pad count when it\'s al padding', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      a[i] = PADDING_VALUE_B;
+    });
+
+    const padding = Troubleshooting.getPadValueAndCount(arrayBuffer);
+
+    expect(padding.count).to.equal(0);
+  });
+
+  it('should remove padding from the start', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      if (i < EXTRA_PADDING_COUNT) {
+        a[i] = PADDING_VALUE_B;
+      } else {
+        a[i] = DATA_VALUE;
+      }
+    });
+
+    const unpaddedArrayBuffer = Troubleshooting.removePaddingFromStart(arrayBuffer, EXTRA_PADDING_COUNT);
+
+    const unpaddedUint8Array = new Uint8Array(unpaddedArrayBuffer);
+
+    unpaddedUint8Array.forEach((e, i, a) => {
+      expect(a[i]).to.equal(DATA_VALUE, `index ${i}`);
+    });
+  });
+
+  it('should remove zero bytes of padding from the start', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      a[i] = DATA_VALUE;
+    });
+
+    const unpaddedArrayBuffer = Troubleshooting.removePaddingFromStart(arrayBuffer, 0);
+
+    const unpaddedUint8Array = new Uint8Array(unpaddedArrayBuffer);
+
+    unpaddedUint8Array.forEach((e, i, a) => {
+      expect(a[i]).to.equal(DATA_VALUE, `index ${i}`);
+    });
+  });
+
+  it('should remove padding from the end', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      if (i < ARRAY_BUFFER_SIZE) {
+        a[i] = DATA_VALUE;
+      } else {
+        a[i] = PADDING_VALUE_B;
+      }
+    });
+
+    const unpaddedArrayBuffer = Troubleshooting.removePaddingFromEnd(arrayBuffer, EXTRA_PADDING_COUNT);
+
+    const unpaddedUint8Array = new Uint8Array(unpaddedArrayBuffer);
+
+    unpaddedUint8Array.forEach((e, i, a) => {
+      expect(a[i]).to.equal(DATA_VALUE, `index ${i}`);
+    });
+  });
+
+  it('should remove zero bytes of padding from the end', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => {
+      a[i] = DATA_VALUE;
+    });
+
+    const unpaddedArrayBuffer = Troubleshooting.removePaddingFromEnd(arrayBuffer, 0);
+
+    const unpaddedUint8Array = new Uint8Array(unpaddedArrayBuffer);
+
+    unpaddedUint8Array.forEach((e, i, a) => {
+      expect(a[i]).to.equal(DATA_VALUE, `index ${i}`);
+    });
+  });
+
+  it('should add padding to start', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => { a[i] = DATA_VALUE; });
+
+    const padding = {
+      value: PADDING_VALUE_B,
+      count: EXTRA_PADDING_COUNT,
+    };
+
+    const paddedArrayBuffer = Troubleshooting.addPaddingToStart(arrayBuffer, padding);
+    expect(paddedArrayBuffer.byteLength).to.equal(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
+
+    const paddedUint8Array = new Uint8Array(paddedArrayBuffer);
+
+    paddedUint8Array.forEach((e, i, a) => {
+      if (i < EXTRA_PADDING_COUNT) {
+        expect(a[i]).to.equal(PADDING_VALUE_B, `index ${i}`);
+      } else {
+        expect(a[i]).to.equal(DATA_VALUE, `index ${i}`);
+      }
+    });
+  });
+
+  it('should add zero bytes of padding to start', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    uint8Array.forEach((e, i, a) => { a[i] = DATA_VALUE; });
+
+    const padding = {
+      value: PADDING_VALUE_B,
+      count: 0,
+    };
+
+    const paddedArrayBuffer = Troubleshooting.addPaddingToStart(arrayBuffer, padding);
+    expect(paddedArrayBuffer.byteLength).to.equal(ARRAY_BUFFER_SIZE);
+
+    const paddedUint8Array = new Uint8Array(paddedArrayBuffer);
+
+    paddedUint8Array.forEach((e, i, a) => { expect(a[i]).to.equal(DATA_VALUE, `index ${i}`); });
+  });
+
   it('should add padding to end', () => {
     const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
     const uint8Array = new Uint8Array(arrayBuffer);
@@ -27,9 +218,9 @@ describe('Troubleshooting', () => {
 
     paddedUint8Array.forEach((e, i, a) => {
       if (i < ARRAY_BUFFER_SIZE) {
-        expect(a[i]).to.equal(DATA_VALUE);
+        expect(a[i]).to.equal(DATA_VALUE, `index ${i}`);
       } else {
-        expect(a[i]).to.equal(PADDING_VALUE_B);
+        expect(a[i]).to.equal(PADDING_VALUE_B, `index ${i}`);
       }
     });
   });
@@ -49,10 +240,10 @@ describe('Troubleshooting', () => {
 
     const paddedUint8Array = new Uint8Array(paddedArrayBuffer);
 
-    paddedUint8Array.forEach((e, i, a) => { expect(a[i]).to.equal(DATA_VALUE); });
+    paddedUint8Array.forEach((e, i, a) => { expect(a[i]).to.equal(DATA_VALUE, `index ${i}`); });
   });
 
-  it('should correctly count padding when there\'s none', () => {
+  it('should count padding when there\'s none', () => {
     const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
     const uint8Array = new Uint8Array(arrayBuffer);
     uint8Array.forEach((e, i, a) => { a[i] = DATA_VALUE; });
@@ -60,7 +251,7 @@ describe('Troubleshooting', () => {
     expect(Troubleshooting.countPadding(arrayBuffer, PADDING_VALUE_A)).to.equal(0);
   });
 
-  it('should correctly count padding when it\'s all padding', () => {
+  it('should count padding when it\'s all padding', () => {
     const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
     const uint8Array = new Uint8Array(arrayBuffer);
     uint8Array.forEach((e, i, a) => { a[i] = PADDING_VALUE_A; });
@@ -68,20 +259,20 @@ describe('Troubleshooting', () => {
     expect(Troubleshooting.countPadding(arrayBuffer, PADDING_VALUE_A)).to.equal(0);
   });
 
-  it('should correctly count padding', () => {
-    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+  it('should count padding', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
     const uint8Array = new Uint8Array(arrayBuffer);
-    uint8Array.forEach((e, i, a) => { a[i] = ((i < ARRAY_BUFFER_SIZE / 2) ? PADDING_VALUE_A : DATA_VALUE); });
+    uint8Array.forEach((e, i, a) => { a[i] = ((i < EXTRA_PADDING_COUNT) ? PADDING_VALUE_A : DATA_VALUE); });
 
-    expect(Troubleshooting.countPadding(arrayBuffer, PADDING_VALUE_A)).to.equal(ARRAY_BUFFER_SIZE / 2);
+    expect(Troubleshooting.countPadding(arrayBuffer, PADDING_VALUE_A)).to.equal(EXTRA_PADDING_COUNT);
   });
 
-  it('should correctly count different values of padding', () => {
-    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE);
+  it('should count different values of padding', () => {
+    const arrayBuffer = new ArrayBuffer(ARRAY_BUFFER_SIZE + EXTRA_PADDING_COUNT);
     const uint8Array = new Uint8Array(arrayBuffer);
-    uint8Array.forEach((e, i, a) => { a[i] = ((i < ARRAY_BUFFER_SIZE / 2) ? PADDING_VALUE_B : DATA_VALUE); });
+    uint8Array.forEach((e, i, a) => { a[i] = ((i < EXTRA_PADDING_COUNT) ? PADDING_VALUE_B : DATA_VALUE); });
 
-    expect(Troubleshooting.countPadding(arrayBuffer, PADDING_VALUE_B)).to.equal(ARRAY_BUFFER_SIZE / 2);
+    expect(Troubleshooting.countPadding(arrayBuffer, PADDING_VALUE_B)).to.equal(EXTRA_PADDING_COUNT);
   });
 
   it('should find the next largest power of 2', () => {
