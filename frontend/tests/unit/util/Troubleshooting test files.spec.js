1e2df0631bfffbe5acb8cf4736843963055e80c8
Euan <euan_forrester@hotmail.com>
2021-05-30 14:26:31 -0700

    Add more test files for the Troubleshooting module

--- frontend/tests/unit/util/Troubleshooting test files.spec.js
+++ frontend/tests/unit/util/Troubleshooting test files.spec.js
@@ -3,8 +3,18 @@ import Troubleshooting from '@/util/Troubleshooting';
 
 import ArrayBufferUtil from '#/util/ArrayBuffer';
 
+/*
+  Files:
+
+  - Tomato Adventure (Japan).srm: Original broken file from a user, which has 128k of padding then 8k of data
+  - Tomato Adventure (Japan) fixed.srm: Corrected version of the above file: the padding is removed and 8k of data remains
+  - Tomato Adventure (Japan) test save all padding.srm: Test save for this game from an emulator, but game wasn't saved yet so file is all padding but is correct size (8k)
+  - Tomato Adventure (Japan) extra padding at end.srm: The original file but with an extra 8k of padding added to the end
+  - Tomato Adventure (Japan) fixed extra padding at end.srm: Corrected version of the original file, but with 8k of padding added to the end
+*/
+
 describe('Troubleshooting example files', () => {
-  it('should fix a file with padding at the start', async () => {
+  it('should remove padding from the start', async () => {
     const brokenSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan).srm');
     const testSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed.srm');
     const correctSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed.srm');
@@ -14,10 +24,30 @@ describe('Troubleshooting example files', () => {
     expect(ArrayBufferUtil.arrayBuffersEqual(fixedData, correctSaveData)).to.equal(true);
   });
 
-  it('should add padding to the start of a file to fix it', async () => {
+  it('should add padding to the start', async () => {
     const brokenSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed.srm');
     const testSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan).srm');
     const correctSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan).srm');
+
+    const fixedData = Troubleshooting.attemptFix(testSaveData, brokenSaveData);
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(fixedData, correctSaveData)).to.equal(true);
+  });
+
+  it('should remove padding at the the start and end', async () => {
+    const brokenSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) extra padding at end.srm');
+    const testSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed.srm');
+    const correctSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed.srm');
+
+    const fixedData = Troubleshooting.attemptFix(testSaveData, brokenSaveData);
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(fixedData, correctSaveData)).to.equal(true);
+  });
+
+  it('should add padding to the end', async () => {
+    const brokenSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan).srm');
+    const testSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed extra padding at end.srm');
+    const correctSaveData = await ArrayBufferUtil.readArrayBuffer('./tests/unit/util/data/Tomato Adventure (Japan) fixed extra padding at end.srm');
 
     const fixedData = Troubleshooting.attemptFix(testSaveData, brokenSaveData);
 
