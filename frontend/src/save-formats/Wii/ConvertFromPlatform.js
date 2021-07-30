b032c5628e1ffb3d401adbd130c04d92172d5335
Euan <euan_forrester@hotmail.com>
2021-07-30 11:31:20 -0700

    Added notes re Neo Geo saves

--- frontend/src/save-formats/Wii/ConvertFromPlatform.js
+++ frontend/src/save-formats/Wii/ConvertFromPlatform.js
@@ -2,11 +2,8 @@
 The Wii save files that we get from the SD card are copied from the Wii's internal NAND memory and then
 encrypted/obfuscated before being written to the SD card.
 
-Once we've decrypted them, we're left with the file as it was stored on the NAND. However, that file may still have
-additional proprietary metadata in it that must also be stripped away before we're left with a raw file that can be
-loaded by a different emulator.
-
-The proprietary metadata depends on the target platform for the save data.
+Once we've decrypted them, we're left with the file as it was stored on the NAND. However, that file may still be
+in a strange format that needs more massaging before it can be loaded by an emulator.
 
 More information can be found here:
 
@@ -50,7 +47,7 @@ export default (arrayBuffer, platformType) => {
 
     case 'VC-C64': // A few of these games offered saving, usually of high scores. Impossible Mission II (PAL regions) offered saving the game. I couldn't find any Wii VC save files online to test with
     case 'VC-SNES': // https://github.com/JanErikGunnar/vcromclaim/blob/master/wiimetadata.py#L482
-    case 'VC-NEOGEO': // https://github.com/JanErikGunnar/vcromclaim/blob/master/wiimetadata.py#L501
+    case 'VC-NEOGEO': // https://github.com/JanErikGunnar/vcromclaim/blob/master/wiimetadata.py#L501 Haven't been able to find any saves online to test this with. Partial (?) list of Neo Geo games that supported saves: https://www.nintendolife.com/forums/virtual_console/neogeo_games_that_allow_saves#reply-05
     case 'VC-Arcade': // Do these even have saves?
     case 'Wii':
     case 'WiiWare':
