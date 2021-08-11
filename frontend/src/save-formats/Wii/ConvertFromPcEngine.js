1d87970cb547313cc3f04a8f55144d0edc84f9e3
Euan <euan_forrester@hotmail.com>
2021-08-11 10:02:32 -0700

    Overwrite the pointer to the next byte after BRAM to reflect that we've truncated the file

--- frontend/src/save-formats/Wii/ConvertFromPcEngine.js
+++ frontend/src/save-formats/Wii/ConvertFromPcEngine.js
@@ -19,9 +19,12 @@
 // However, the Wii files are 8kB in size and correspondingly they write 0xA000 (0x8000 + 8 kB) to those bytes in their output.
 // So when we truncate the file to 2kB we need to update those values as well.
 
+const LITTLE_ENDIAN = true;
 const BLOCK_SIZE = 4;
 const NUM_BLOCKS_PER_SET = 4;
-const BRAM_SIZE = 2048; // On a real PCE many games would share this memory. Most emulators, though, create a new virtual BRAM for each game
+const BRAM_MEMORY_ADDRESS = 0x8000; // The address in the CPU memory space that the BRAM is mapped to (see above)
+const BRAM_SIZE = 2048; // In bytes. On a real PCE many games would share this memory. Most emulators, though, create a new virtual BRAM for each game
+const POINTER_TO_FIRST_BYTE_AFTER_BRAM_OFFSET = 4; // Offset in BRAM of the pointer to the first byte in CPU memory after BRAM
 const HEADER_LENGTH = 16;
 const SEED1_START = 4;
 const FOOTER_LENGTH = 16;
@@ -166,9 +169,12 @@ export default (arrayBuffer) => {
 
   // Return data truncated to the correct size
 
-  // FIXME: We need to change the first 2 bytes of the second block when we truncate the file. They indicate the next address after
-  // BRAM (which is mapped to address 0x8000), so if we only have 2kB instead of 8kB we need to change those bytes from 0xa000 to 0x8800
-  // The file is in little endian.
+  // We need to change the pointer to the first byte after BRAM when we truncate the file. The Wii version of the file provides
+  // 8kB of BRAM (and so this pointer contains 0x8000 + 8192 = 0xA000). But for compatibility with other emulators we're going to
+  // truncate the file to 2kB: the size of BRAM in a normal PC Engine. So we'll change this value to 0x8000 + 2048 = 0x8800.
+
+  const outputDataView = new DataView(outputArrayBuffer);
+  outputDataView.setUint16(POINTER_TO_FIRST_BYTE_AFTER_BRAM_OFFSET, BRAM_MEMORY_ADDRESS + BRAM_SIZE, LITTLE_ENDIAN);
 
   return {
     saveData: outputArrayBuffer.slice(0, BRAM_SIZE),
