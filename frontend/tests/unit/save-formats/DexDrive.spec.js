24f21bd648bec9e99d30e594d9ce72ee9e5f0fa5
Euan <euan_forrester@hotmail.com>
2021-08-26 11:45:44 -0700

    Added extraction of the file description

--- frontend/tests/unit/save-formats/DexDrive.spec.js
+++ frontend/tests/unit/save-formats/DexDrive.spec.js
@@ -31,7 +31,8 @@ describe('DexDrive PS1 save format', () => {
 
     for (let i = 0; i < dexDriveSaveData.getSaveFiles().length; i += 1) {
       const saveFile = dexDriveSaveData.getSaveFiles()[i];
-      console.log(`Found block ${saveFile.block} contains file '${saveFile.filename}' with comment '${saveFile.comment}' and raw save data length ${saveFile.rawData.byteLength}`);
+      console.log(`Found block ${saveFile.block} contains file: '${saveFile.filename}', comment: '${saveFile.comment}', \
+description: '${saveFile.description}', raw save data length: ${saveFile.rawData.byteLength}`);
 
       ArrayBufferUtil.writeArrayBuffer(RAW_ONE_FILE_FILENAME, saveFile.rawData);
     }
