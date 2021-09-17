43e02a17c9f3513f0f5d1dcd71d7b232591555ef
Euan <euan_forrester@hotmail.com>
2021-09-16 18:35:38 -0700

    Fixed wrong sized file being downloaded

--- frontend/src/components/ConvertPs1DexDrive.vue
+++ frontend/src/components/ConvertPs1DexDrive.vue
@@ -166,7 +166,7 @@ export default {
     },
     convertFile() {
       console.log(`Trying to upload save data at index ${this.selectedSaveData}`);
-      const outputArrayBuffer = (this.conversionDirection === 'convertToEmulator') ? this.dexDriveSaveData.getSaveFiles()[this.selectedSaveData] : this.dexDriveSaveData.getArrayBuffer();
+      const outputArrayBuffer = (this.conversionDirection === 'convertToEmulator') ? this.dexDriveSaveData.getSaveFiles()[this.selectedSaveData].rawData : this.dexDriveSaveData.getArrayBuffer();
 
       const outputBlob = new Blob([outputArrayBuffer], { type: 'application/octet-stream' });
 
