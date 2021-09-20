b2c11fb1b23cc31d8759024dc14120bdd2b60d4a
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2021-09-20 13:20:17 -0700

    Added UI for reading and creating PS1 emulator saves (#45)
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/tests/unit/save-formats/PS1/Memcard.spec.js
+++ frontend/tests/unit/save-formats/PS1/Memcard.spec.js
@@ -8,6 +8,11 @@ const SINGLE_BLOCK_TWO_FILES_FILENAMES = [`${DIR}/castlevania-symphony-of-the-ni
 
 const MULTI_BLOCK_TWO_FILES_FILENAMES = [`${DIR}/gran-turismo.26537-BASCUS-94194GT.srm`, `${DIR}/gran-turismo.26537-BASCUS-94194RT.srm`];
 
+const SINGLE_FILE_SAVE_FILENAME = `${DIR}/Castlevania - Symphony of the Night (USA).0.mcr`;
+const RAW_SINGLE_FILE_FILENAME = `${DIR}/Castlevania - Symphony of the Night (USA).0.BASLUS-00067DRAX00.srm`;
+
+const NO_FILES_SAVE_FILENAME = `${DIR}/Castlevania - Symphony of the Night (USA).1.mcr`;
+
 describe('PS1 memcard save format', () => {
   it('should correctly create a file that has two saves of one block each', async () => {
     const saveFilesArrayBuffers = await Promise.all(SINGLE_BLOCK_TWO_FILES_FILENAMES.map((n) => ArrayBufferUtil.readArrayBuffer(n)));
@@ -47,5 +52,27 @@ describe('PS1 memcard save format', () => {
     expect(ps1MemcardSaveData.getSaveFiles()[1].filename).to.equal('BASCUS-94194RT');
     expect(ps1MemcardSaveData.getSaveFiles()[1].description).to.equal('ＧＴ　ｒｅｐｌａｙ　ｄａｔａ');
     expect(ArrayBufferUtil.arrayBuffersEqual(ps1MemcardSaveData.getSaveFiles()[1].rawData, saveFilesArrayBuffers[1])).to.equal(true);
+  });
+
+  it('should load a file containing no savese', async () => {
+    const saveFileArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NO_FILES_SAVE_FILENAME);
+
+    const ps1MemcardSaveData = Ps1MemcardSaveData.createFromPs1MemcardData(saveFileArrayBuffer);
+
+    expect(ps1MemcardSaveData.getSaveFiles().length).to.equal(0);
+  });
+
+  it('should load a file containing a single save', async () => {
+    const saveFileArrayBuffer = await ArrayBufferUtil.readArrayBuffer(SINGLE_FILE_SAVE_FILENAME);
+    const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_SINGLE_FILE_FILENAME);
+
+    const ps1MemcardSaveData = Ps1MemcardSaveData.createFromPs1MemcardData(saveFileArrayBuffer);
+
+    expect(ps1MemcardSaveData.getSaveFiles().length).to.equal(1);
+
+    expect(ps1MemcardSaveData.getSaveFiles()[0].startingBlock).to.equal(0);
+    expect(ps1MemcardSaveData.getSaveFiles()[0].filename).to.equal('BASLUS-00067DRAX00');
+    expect(ps1MemcardSaveData.getSaveFiles()[0].description).to.equal('ＣＡＳＴＬＥＶＡＮＩＡ－１　ＥＵＡＮ　２％');
+    expect(ArrayBufferUtil.arrayBuffersEqual(ps1MemcardSaveData.getSaveFiles()[0].rawData, rawArrayBuffer)).to.equal(true);
   });
 });
