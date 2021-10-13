ccb6ab3f0ba0f997c51f8d4acf12abe0a5860e0f
Euan <euan_forrester@hotmail.com>
2021-10-13 14:57:44 -0700

    Added basics of creating a mempack file: ID area, and joining together all the pages

--- frontend/src/save-formats/N64/Mempack.js
+++ frontend/src/save-formats/N64/Mempack.js
@@ -1,4 +1,4 @@
-/* eslint no-bitwise: ['error', { 'allow': ['&', '^', '&=', '^=', '|='] }] */
+/* eslint no-bitwise: ['error', { 'allow': ['&', '|', '^', '&=', '^=', '|='] }] */
 
 /*
 The N64 mempack format is described here:
@@ -30,10 +30,15 @@ const INODE_TABLE_BACKUP_PAGE = 2; // Page 2 is a repeat of page 1, checked in c
 const NOTE_TABLE_PAGES = [3, 4];
 const FIRST_SAVE_DATA_PAGE = 5;
 
+const ID_AREA_BLOCK_SIZE = 32;
 const ID_AREA_CHECKSUM_OFFSETS = [0x20, 0x60, 0x80, 0xC0]; // 4 different checksum in the ID Area, and if any of them match then the data is deemed valid
 const ID_AREA_CHECKSUM_LENGTH = 28;
+const ID_AREA_DEVICE_OFFSET = 25;
+const ID_AREA_BANK_SIZE_OFFSET = 26;
 const ID_AREA_CHECKSUM_DESIRED_SUM_A_OFFSET = 28;
 const ID_AREA_CHECKSUM_DESIRED_SUM_B_OFFSET = 30;
+const DEVICE_CONTROLLER_PAK = 0x1;
+const BANK_SIZE = 0x1;
 
 const NOTE_TABLE_BLOCK_SIZE = 32;
 const NOTE_TABLE_GAME_SERIAL_CODE_OFFSET = 0;
@@ -70,11 +75,39 @@ function getNextPageNumber(inodePageDataView, pageNumber) {
   return inodePageDataView.getUint16(pageNumber * 2, LITTLE_ENDIAN);
 }
 
+function createEmptyPage() {
+  const arrayBuffer = new ArrayBuffer(PAGE_SIZE);
+  const array = new Uint8Array(arrayBuffer);
+
+  array.fill(0);
+
+  return arrayBuffer;
+}
+
 function getStringCode(uint8Array) {
   // Made to be compatible with the strings fed to https://github.com/bryc/mempak/blob/master/js/codedb.js in case we want to use those lookup tables.
   // Note that there's only 1 code there (the publisher for Wave Race 64) that actually uses the - at this time,
   // but may as well make the output here match exactly just in case.
   return String.fromCharCode(...uint8Array).replace(/\0/g, '-');
+}
+
+function calculateChecksumsOfBlock(arrayBuffer) {
+  const dataView = new DataView(arrayBuffer);
+
+  let sumA = 0x0;
+  let sumB = 0xFFF2;
+
+  for (let i = 0; i < ID_AREA_CHECKSUM_LENGTH; i += 2) {
+    sumA += dataView.getUint16(i, LITTLE_ENDIAN);
+    sumA &= 0xFFFF;
+  }
+
+  sumB -= sumA;
+
+  return {
+    sumA,
+    sumB,
+  };
 }
 
 // Taken from https://github.com/bryc/mempak/blob/master/js/parser.js#L147
@@ -90,15 +123,8 @@ function checkIdArea(arrayBuffer) {
   const array = new Uint8Array(arrayBuffer);
 
   ID_AREA_CHECKSUM_OFFSETS.forEach((offset) => {
-    let sumA = 0x0;
-    let sumB = 0xFFF2;
-
-    for (let i = 0; i < ID_AREA_CHECKSUM_LENGTH; i += 2) {
-      sumA += dataView.getUint16(offset + i, LITTLE_ENDIAN);
-      sumA &= 0xFFFF;
-    }
-
-    sumB -= sumA;
+    const block = arrayBuffer.slice(offset, offset + ID_AREA_BLOCK_SIZE);
+    const checksums = calculateChecksumsOfBlock(block);
 
     const desiredSumA = dataView.getUint16(offset + ID_AREA_CHECKSUM_DESIRED_SUM_A_OFFSET, LITTLE_ENDIAN);
     let desiredSumB = dataView.getUint16(offset + ID_AREA_CHECKSUM_DESIRED_SUM_B_OFFSET, LITTLE_ENDIAN);
@@ -107,17 +133,59 @@ function checkIdArea(arrayBuffer) {
     // https://github.com/bryc/mempak/blob/master/js/parser.js#L127
     //
     // FIXME: Note that here we're operating on a copy of the data (a slice) and so this won't affect what's written out
-    if ((desiredSumB !== sumB) && ((desiredSumB ^ 0x0C) === sumB) && (desiredSumA === sumA)) {
+    if ((desiredSumB !== checksums.sumB) && ((desiredSumB ^ 0x0C) === checksums.sumB) && (desiredSumA === checksums.sumA)) {
       desiredSumB ^= 0xC;
       array[offset + 31] ^= 0xC;
     }
 
-    foundValidBlock = foundValidBlock || ((desiredSumA === sumA) && (desiredSumB === sumB));
+    foundValidBlock = foundValidBlock || ((desiredSumA === checksums.sumA) && (desiredSumB === checksums.sumB));
   });
 
   if (!foundValidBlock) {
     throw new Error('File appears to be corrupt - checksums in ID Area do not match');
   }
+}
+
+function randomByte() {
+  return 0 | Math.random() * 256;
+}
+
+// Based on https://github.com/bryc/mempak/blob/master/js/state.js#L13
+function createIdAreaPage() {
+  // This page is 4 copies of the same block at different offsets
+
+  const checksumBlock = new ArrayBuffer(ID_AREA_BLOCK_SIZE);
+  const checksumBlockDataView = new DataView(checksumBlock);
+
+  checksumBlockDataView.setUint8(1, randomByte() & 0x3F);
+  checksumBlockDataView.setUint8(5, randomByte() & 0x7);
+  checksumBlockDataView.setUint8(6, randomByte());
+  checksumBlockDataView.setUint8(7, randomByte());
+  checksumBlockDataView.setUint8(8, randomByte() & 0xF);
+  checksumBlockDataView.setUint8(9, randomByte());
+  checksumBlockDataView.setUint8(10, randomByte());
+  checksumBlockDataView.setUint8(11, randomByte());
+  checksumBlockDataView.setUint8(ID_AREA_DEVICE_OFFSET, DEVICE_CONTROLLER_PAK);
+  checksumBlockDataView.setUint8(ID_AREA_BANK_SIZE_OFFSET, BANK_SIZE);
+
+  const checksums = calculateChecksumsOfBlock(checksumBlock);
+
+  checksumBlockDataView.setUint16(ID_AREA_CHECKSUM_DESIRED_SUM_A_OFFSET, checksums.sumA, LITTLE_ENDIAN);
+  checksumBlockDataView.setUint16(ID_AREA_CHECKSUM_DESIRED_SUM_B_OFFSET, checksums.sumB, LITTLE_ENDIAN);
+
+  // Now we can make our empty page
+
+  let pageArrayBuffer = new ArrayBuffer(PAGE_SIZE);
+
+  pageArrayBuffer = Util.fillArrayBuffer(pageArrayBuffer, 0);
+
+  // Now copy our block to the various offsets it needs to be at
+
+  ID_AREA_CHECKSUM_OFFSETS.forEach((offset) => {
+    pageArrayBuffer = Util.setArrayBufferPortion(pageArrayBuffer, checksumBlock, offset, 0, ID_AREA_BLOCK_SIZE);
+  });
+
+  return pageArrayBuffer;
 }
 
 // Taken from https://github.com/bryc/mempak/blob/master/js/parser.js#L173
@@ -200,6 +268,10 @@ function readNoteTable(inodePageArrayBuffer, noteTableArrayBuffer) {
     noteKeys,
     notes,
   };
+}
+
+function createNoteTablePage() {
+  return new ArrayBuffer(PAGE_SIZE);
 }
 
 // Taken from https://github.com/bryc/mempak/blob/master/js/parser.js#L269
@@ -305,6 +377,10 @@ function checkIndexes(inodeArrayBuffer, noteTableKeys) {
   return noteIndexes;
 }
 
+function createInodeTablePage() {
+  return new ArrayBuffer(PAGE_SIZE);
+}
+
 export default class N64MempackSaveData {
   static NUM_NOTES = NUM_NOTES;
 
@@ -313,7 +389,43 @@ export default class N64MempackSaveData {
   }
 
   static createFromSaveFiles(saveFiles) {
-    this.saveFiles = saveFiles;
+    // Check to make sure that there's not too many save files, or too much data, or save files the wrong length
+
+    if (saveFiles.length > NUM_NOTES) {
+      throw new Error(`Found ${saveFiles.length} notes, but max is ${NUM_NOTES}`);
+    }
+
+    const totalSize = saveFiles.reduce((accumulator, x) => accumulator + x.rawData.byteLength);
+    const maxSize = (NUM_PAGES - FIRST_SAVE_DATA_PAGE) * PAGE_SIZE;
+
+    if (totalSize > maxSize) {
+      throw new Error(`Total size of notes is ${totalSize} bytes, but max is ${maxSize}`);
+    }
+
+    saveFiles.forEach((x) => {
+      if (x.rawData.byteLength % PAGE_SIZE !== 0) {
+        throw new Error(`All saves must be multiples of ${PAGE_SIZE} bytes, but save '${x.noteName}' is ${x.rawData.byteLength} bytes`);
+      }
+    });
+
+    // Now make our header pages
+
+    const idAreaPage = createIdAreaPage();
+    const noteTablePage = createNoteTablePage(saveFiles);
+    const inodeTablePage = createInodeTablePage(noteTablePage, saveFiles);
+
+    // Technically, we should split each save into separate pages, then concat all 128 pages together to get the final
+    // arraybuffer. But, we can cheat and just concat the existing saves together instead of splitting them apart first
+
+    let dataPages = Util.concatArrayBuffers(saveFiles.map((x) => x.rawData));
+
+    while (dataPages.byteLength < maxSize) {
+      dataPages = Util.concatArrayBuffers(dataPages, createEmptyPage());
+    }
+
+    const arrayBuffer = Util.concatArrayBuffers([idAreaPage, inodeTablePage, inodeTablePage, noteTablePage, dataPages]);
+
+    return new N64MempackSaveData(arrayBuffer);
   }
 
   constructor(mempackArrayBuffer) {
