800687a06d0adaee8876b0ea17e6249901be013d
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2021-10-30 21:23:46 -0700

    Switch to using plugin for Google Tag Manager, and disable it in dev (#61)
    * Add in Google Tag Manager plugin, and remove stuff baked into index.html
    
    * Enable/disable GTM and its debug prints based on dev vs prod build
    
    * Remove console logs
    
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import './plugins/bootstrap-vue';
 import './plugins/fontawesome-vue';
 import './plugins/mediaquery-vue';
+import './plugins/google-tag-manager-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
---
+++ frontend/src/plugins/google-tag-manager-vue.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueGtm from '@gtm-support/vue2-gtm';
+import router from '../router/index';
+
+Vue.use(VueGtm, {
+  id: 'GTM-MFQ82X4',
+  defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+  compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
+  enabled: process.env.VUE_APP_ENABLE_GOOGLE_TAG_MANAGER === 'true',
+  debug: process.env.VUE_APP_DEBUG_GOOGLE_TAG_MANAGER === 'true', // Whether or not display console logs debugs (optional)
+  loadScript: true,
+  vueRouter: router,
+});
