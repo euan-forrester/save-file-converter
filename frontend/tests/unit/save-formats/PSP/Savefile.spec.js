ef2db02accb2abe9fbc7c895dcd560b93422cbad
Euan <euan_forrester@hotmail.com>
2021-12-16 13:58:07 -0800

    More fixes for test

--- frontend/tests/unit/save-formats/PSP/Savefile.spec.js
+++ frontend/tests/unit/save-formats/PSP/Savefile.spec.js
@@ -1,4 +1,4 @@
-// import { expect } from 'chai';
+import { expect } from 'chai';
 import PspSaveData from '@/save-formats/PSP/Savefile';
 import ArrayBufferUtil from '#/util/ArrayBuffer';
 
@@ -7,7 +7,7 @@ const DIR = './tests/unit/save-formats/data/psp';
 const GAME_KEY = Buffer.from('01020304050607080900010203040506', 'hex');
 
 const ENCRYPTED_FILENAME = `${DIR}/DRACULA.BIN`;
-const UNENCRYPTED_FILENAME = `${DIR}/DRACULA-unencrypted.BIN`;
+// const UNENCRYPTED_FILENAME = `${DIR}/DRACULA-unencrypted.BIN`;
 
 describe('PSP save decryption', () => {
   it('should decrypt an encrypted PSP save file', async () => {
@@ -16,7 +16,9 @@ describe('PSP save decryption', () => {
 
     const pspSaveData = await PspSaveData.createFromEncryptedData(encryptedArrayBuffer, GAME_KEY);
 
-    //ArrayBufferUtil.writeArrayBuffer(UNENCRYPTED_FILENAME, pspSaveData.getUnencryptedArrayBuffer());
+    expect(pspSaveData.getUnencryptedArrayBuffer().byteLength).to.not.equal(0);
+
+    // ArrayBufferUtil.writeArrayBuffer(UNENCRYPTED_FILENAME, pspSaveData.getUnencryptedArrayBuffer());
 
     // expect(ArrayBufferUtil.arrayBuffersEqual(pspSaveData.getUnencryptedArrayBuffer(), unencryptedArrayBuffer)).to.equal(true);
   });
