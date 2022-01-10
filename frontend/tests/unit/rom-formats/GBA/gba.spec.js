b226463502acef1b93b087d269d83a930f1849dd
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2022-01-10 13:05:55 -0800

    Test refactors (#78)
    * Moved GBA test ROM to rom-files folder
    
    * Move PSP ISO test file
    
    * Added test for GBA ROM format
    
    * Added test for re-decrypting a PSP save file that was encrypted
    
    Co-authored-by: Euan <euan_forrester@hotmail.com>

---
+++ frontend/tests/unit/rom-formats/GBA/gba.spec.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import GbaRom from '@/rom-formats/gba';
+import ArrayBufferUtil from '#/util/ArrayBuffer';
+
+const DIR = './tests/unit/rom-formats/data/gba';
+
+const FILENAME = `${DIR}/Zelda - Minish Cap header.gba`; // Not a real ROM file (!), just the parts in the header that we read
+
+describe('GBA ROM format', () => {
+  it('should parse a GBA ROM', async () => {
+    const romArrayBuffer = await ArrayBufferUtil.readArrayBuffer(FILENAME);
+
+    const gbaRom = new GbaRom(romArrayBuffer);
+
+    expect(gbaRom.getInternalName()).to.equal('GBAZELDA MC\u0000BZME');
+    expect(gbaRom.getChecksum()).to.equal(0);
+    expect(gbaRom.getComplimentCheck()).to.equal(216);
+    expect(gbaRom.getMaker()).to.equal(48);
+  });
+});
