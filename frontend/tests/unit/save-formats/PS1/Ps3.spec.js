bd56fdc0b7804eeae2c1be7b1045cef8748f1f94
Euan <euan_forrester@hotmail.com>
2022-02-17 13:20:21 -0800

    Replaced file for PS3 test with one where I got the result confirmed working in an emulator

--- frontend/tests/unit/save-formats/PS1/Ps3.spec.js
+++ frontend/tests/unit/save-formats/PS1/Ps3.spec.js
@@ -4,9 +4,9 @@ import ArrayBufferUtil from '#/util/ArrayBuffer';
 
 const DIR = './tests/unit/save-formats/data/ps1/ps3';
 
-const PS3_STREET_FIGHTER_EX2_PLUS_FILENAME = `${DIR}/Street Fighter EX2 Plus-BASLUS-011054441544131.PSV`;
-const PS3_STREET_FIGHTER_EX2_PLUS_BASE_FILENAME = PS3_STREET_FIGHTER_EX2_PLUS_FILENAME.substr(-26, 22);
-const RAW_STREET_FIGHTER_EX2_PLUS_FILENAME = `${DIR}/street-fighter-ex2-plus.17422.mcr`;
+const PS3_STREET_FIGHTER_EX2_PLUS_FILENAME = `${DIR}/Street Fighter EX2 Plus-BASLUS-0110553595354454D.PSV`;
+const PS3_STREET_FIGHTER_EX2_PLUS_BASE_FILENAME = PS3_STREET_FIGHTER_EX2_PLUS_FILENAME.substr(-28, 24);
+const RAW_STREET_FIGHTER_EX2_PLUS_FILENAME = `${DIR}/street-fighter-ex2-plus.17782.mcr`;
 
 const RAW_SUIKODEN_2_FILENAME = `${DIR}/Suikoden 2-BASLUS-00958GS2-7.srm`;
 const RAW_SUIKODEN_2_BASE_FILENAME = RAW_SUIKODEN_2_FILENAME.substr(-21, 17);
@@ -15,7 +15,7 @@ const PS3_SUIKODEN_2_FILENAME = `${DIR}/Suikoden 2-BASLUS-009584753322D37.PSV`;
 describe('PS1 - PS3 save format', () => {
   it('should convert a PS3 file to PS1, which contains a Street Fighter EX2 Plus save', async () => {
     const ps3SaveArrayBuffer = await ArrayBufferUtil.readArrayBuffer(PS3_STREET_FIGHTER_EX2_PLUS_FILENAME);
-    // const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_STREET_FIGHTER_EX2_PLUS);
+    const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_STREET_FIGHTER_EX2_PLUS_FILENAME);
 
     const ps3SaveFiles = [
       {
@@ -29,7 +29,7 @@ describe('PS1 - PS3 save format', () => {
     expect(ps3SaveData.getSaveFiles().length).to.equal(1);
     expect(ps3SaveData.getPs3SaveFiles().length).to.equal(1);
 
-    ArrayBufferUtil.writeArrayBuffer(RAW_STREET_FIGHTER_EX2_PLUS_FILENAME, ps3SaveData.getMemoryCard().getArrayBuffer());
+    expect(ArrayBufferUtil.arrayBuffersEqual(ps3SaveData.getMemoryCard().getArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
   it('should convert a PS1 file to PS3 file, which contains a Suikoden 2 save', async () => {
