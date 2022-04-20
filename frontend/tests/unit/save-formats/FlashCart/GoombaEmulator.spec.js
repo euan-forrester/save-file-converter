30c263fe2ed047d391c8d7e9cb18290ff1b8f161
Euan <euan_forrester@hotmail.com>
2022-04-20 16:58:46 -0700

    Added test of file from GBA Everdrive

--- frontend/tests/unit/save-formats/FlashCart/GoombaEmulator.spec.js
+++ frontend/tests/unit/save-formats/FlashCart/GoombaEmulator.spec.js
@@ -15,8 +15,13 @@ const GOOMBA_ZELDA_FILENAME = `${DIR}/Legend of Zelda, The - Link's Awakening (U
 const ZELDA_ROM_FILENAME = `${DIR}/retail/Legend of Zelda, The - Link's Awakening (USA, Europe).gb`;
 const ZELDA_ROM_CHECKSUM = 0x377aa60f;
 
+const RAW_WARIO_FILENAME = `${DIR}/Wario Land 3.sav`;
+const GOOMBA_WARIO_FILENAME = `${DIR}/Wario Land 3.srm`;
+
+const WARIO_ROM_CHECKSUM = 0xb2380b51;
+
 describe('Flash cart - Goomba emulator save format', () => {
-  it('should convert a Goomba emulator save to raw format', async () => {
+  it('should convert a Goomba emulator save made with an EZ Flash ODE to raw format', async () => {
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_ZELDA_FILENAME);
     const goombaArrayBuffer = await ArrayBufferUtil.readArrayBuffer(GOOMBA_ZELDA_FILENAME);
 
@@ -27,6 +32,20 @@ describe('Flash cart - Goomba emulator save format', () => {
     expect(goombaEmulatorSaveData.getFrameCount()).to.equal(0); // Dunno what this means
     expect(goombaEmulatorSaveData.getGameTitle()).to.equal('ZELDA');
     expect(goombaEmulatorSaveData.getCompressedSize()).to.equal(196);
+    expect(goombaEmulatorSaveData.getUncompressedSize()).to.equal(rawArrayBuffer.byteLength);
+  });
+
+  it('should convert a Goomba emulator save made with an Everdrive GBA to raw format', async () => {
+    const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_WARIO_FILENAME);
+    const goombaArrayBuffer = await ArrayBufferUtil.readArrayBuffer(GOOMBA_WARIO_FILENAME);
+
+    const goombaEmulatorSaveData = GoombaEmulatorSaveData.createFromGoombaData(goombaArrayBuffer);
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(goombaEmulatorSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
+    expect(goombaEmulatorSaveData.getRomChecksum()).to.equal(WARIO_ROM_CHECKSUM);
+    expect(goombaEmulatorSaveData.getFrameCount()).to.equal(0); // Dunno what this means
+    expect(goombaEmulatorSaveData.getGameTitle()).to.equal('WARIOLAND3');
+    expect(goombaEmulatorSaveData.getCompressedSize()).to.equal(5416);
     expect(goombaEmulatorSaveData.getUncompressedSize()).to.equal(rawArrayBuffer.byteLength);
   });
 
