cb77b7d5f9d00f925f53bbdfe3a8664e02037442
Euan <euan_forrester@hotmail.com>
2022-04-29 14:13:54 -0700

    Added parsing of GBC ROMs

--- frontend/src/rom-formats/gb.js
+++ frontend/src/rom-formats/gb.js
@@ -1,16 +1,26 @@
 // Extracts some information from a Game Boy ROM.
+// Information can be found here: https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header#0134-0143_-_Title
 
 import Util from '../util/util';
 
 const INTERNAL_NAME_OFFSET = 0x134;
-const INTERNAL_NAME_LENGTH = 0x13;
+const INTERNAL_NAME_LENGTH_GB = 0x10;
+const INTERNAL_NAME_LENGTH_GBC = 0xB;
 const INTERNAL_NAME_ENCODING = 'US-ASCII';
+
+const GBC_FLAG_OFFSET = 0x143;
+const GBC_FLAG_GB_AND_GBC = 0x80;
+const GBC_FLAG_GBC_ONLY = 0xC0;
 
 export default class GbRom {
   constructor(romArrayBuffer) {
     const uint8Array = new Uint8Array(romArrayBuffer);
 
-    this.internalName = Util.readNullTerminatedString(uint8Array, INTERNAL_NAME_OFFSET, INTERNAL_NAME_ENCODING, INTERNAL_NAME_LENGTH);
+    this.isGbc = (uint8Array[GBC_FLAG_OFFSET] === GBC_FLAG_GB_AND_GBC) || (uint8Array[GBC_FLAG_OFFSET] === GBC_FLAG_GBC_ONLY);
+
+    const internalNameLength = this.isGbc ? INTERNAL_NAME_LENGTH_GBC : INTERNAL_NAME_LENGTH_GB;
+
+    this.internalName = Util.readNullTerminatedString(uint8Array, INTERNAL_NAME_OFFSET, INTERNAL_NAME_ENCODING, internalNameLength);
     this.romArrayBuffer = romArrayBuffer;
   }
 
@@ -20,5 +30,9 @@ export default class GbRom {
 
   getRomArrayBuffer() {
     return this.romArrayBuffer;
+  }
+
+  getIsGbc() {
+    return this.isGbc;
   }
 }
--- frontend/tests/unit/rom-formats/GB/gb.spec.js
+++ frontend/tests/unit/rom-formats/GB/gb.spec.js
@@ -4,14 +4,25 @@ import ArrayBufferUtil from '#/util/ArrayBuffer';
 
 const DIR = './tests/unit/rom-formats/data/gb';
 
-const FILENAME = `${DIR}/Zelda - Link's Awakening header.gb`; // Not a real ROM file (!), just the parts in the header that we read
+const GB_FILENAME = `${DIR}/Zelda - Link's Awakening header.gb`; // Not a real ROM file (!), just the parts in the header that we read
+const GBC_FILENAME = `${DIR}/Wario Land 3 header.gbc`; // Not a real ROM file (!), just the parts in the header that we read
 
-describe('GB ROM format', () => {
+describe('GB/GBC ROM format', () => {
   it('should parse a GB ROM', async () => {
-    const romArrayBuffer = await ArrayBufferUtil.readArrayBuffer(FILENAME);
+    const romArrayBuffer = await ArrayBufferUtil.readArrayBuffer(GB_FILENAME);
 
     const gbRom = new GbRom(romArrayBuffer);
 
     expect(gbRom.getInternalName()).to.equal('ZELDA');
+    expect(gbRom.getIsGbc()).to.equal(false);
+  });
+
+  it('should parse a GBC ROM', async () => {
+    const romArrayBuffer = await ArrayBufferUtil.readArrayBuffer(GBC_FILENAME);
+
+    const gbRom = new GbRom(romArrayBuffer);
+
+    expect(gbRom.getInternalName()).to.equal('WARIOLAND3');
+    expect(gbRom.getIsGbc()).to.equal(true);
   });
 });
