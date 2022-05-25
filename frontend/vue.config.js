f4442fd781fb46b390b07de1d48d8805ce84ce6c
Euan <euan_forrester@hotmail.com>
2022-05-25 15:19:48 -0700

    Fixed Wii decrypting with the new webpack

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -54,7 +54,7 @@ module.exports = {
       },
       fallback: {
         "fs": false,
-        "stream": false, // or require.resolve("stream-browserify"), and yarn install stream-browserify
+        "stream": require.resolve("stream-browserify"), // https://github.com/crypto-browserify/cipher-base/issues/11
         "buffer": require.resolve("buffer"), // https://stackoverflow.com/a/68723223
       }
     },
