875c13bc02ee126fa9b89b93a483edb1a05f109a
Euan <euan_forrester@hotmail.com>
2022-05-27 12:05:18 -0700

    Updated popup message for output filesize

--- frontend/src/components/OutputFilesize.vue
+++ frontend/src/components/OutputFilesize.vue
@@ -8,8 +8,8 @@
           :options="options"
         />
         <help-button
-          popover-text="Some save files found on the Internet are not the correct size for their corresponding game and so may not work on a real cartridge or with a particular emulator.
-          Try creating a test save with your cartridge or emulator to find what size it expects, then adjust the value here if necessary."
+          popover-text="Some cartridges, flash carts, or emulators may not accept saves of a particular size for a particular game.
+          Try creating a test save with your cartridge, flash cart, or emulator to find what size it expects, then adjust the value here if necessary."
           :id="this.id"
           class="help-button"
         />
