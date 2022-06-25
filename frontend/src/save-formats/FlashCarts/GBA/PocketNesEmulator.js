03d64cd932975c764fb5332e04990fd6251d5031
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2022-06-25 15:12:23 -0700

    PocketNES: matched config values to emulator defaults (#131)
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/save-formats/FlashCarts/GBA/PocketNesEmulator.js
+++ frontend/src/save-formats/FlashCarts/GBA/PocketNesEmulator.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-bitwise */
+
 // Find it at https://github.com/Dwedit/PocketNES/releases
 
 import EmulatorBase from './EmulatorBase';
@@ -9,6 +11,7 @@ const POCKETNES_MAGIC = 0x57A731D7; // Pocket NES save
 const POCKETNES_CONFIG_DATA_SIZE_OFFSET = 0;
 const POCKETNES_CONFIG_DATA_TYPE_OFFSET = 2;
 const POCKETNES_CONFIG_DATA_DISPLAY_TYPE_OFFSET = 4;
+const POCKETNES_CONFIG_DATA_MISC_OFFSET = 5;
 const POCKETNES_CONFIG_DATA_SRAM_ROM_CHECKSUM_OFFSET = 8;
 const POCKETNES_CONFIG_DATA_RESERVED_OFFSET = 16;
 const POCKETNES_CONFIG_DATA_RESERVED_LENGTH = 32;
@@ -16,7 +19,27 @@ const POCKETNES_CONFIG_DATA_RESERVED_DATA = 'CFG';
 const POCKETNES_CONFIG_DATA_RESERVED_ENCODING = 'US-ASCII';
 const POCKETNES_CONFIG_DATA_LENGTH = POCKETNES_CONFIG_DATA_RESERVED_OFFSET + POCKETNES_CONFIG_DATA_RESERVED_LENGTH;
 
-const POCKETNES_CONFIG_DATA_DEFAULT_DISPLAY_TYPE = 0;
+// DisplayType explanation:
+//
+// The lower bits are the scaling type and the upper bits are the gamma: https://github.com/Dwedit/PocketNES/blob/master/src/sram.c#L1119
+//
+// The default scaling value is 3 (everything scaled): https://github.com/Dwedit/PocketNES/blob/master/src/main.c#L83
+// The various values are described here: https://github.com/Dwedit/PocketNES/blob/master/src/ui.c#L194
+// The default gamma is 0
+//
+// The default value of flicker is 1, and it is stored in the misc field: https://github.com/Dwedit/PocketNES/blob/master/src/sram.c#L1044
+//
+// The default follow by value is 0, and the default follow sprite is 0. I don't see an obvious place where they're stored
+
+const DEFAULT_SCALING = 3;
+const DEFAULT_GAMMA = 0;
+
+const DEFAULT_AUTOSLEEP_TIME = 0;
+const DEFAULT_AUTOSTATE = 0;
+const DEFAULT_FLICKER = 1;
+
+const POCKETNES_CONFIG_DATA_DEFAULT_DISPLAY_TYPE = ((DEFAULT_GAMMA & 0x7) << 5) | (DEFAULT_SCALING & 0xF); // https://github.com/Dwedit/PocketNES/blob/master/src/sram.c#L1037
+const POCKETNES_CONFIG_DATA_DEFAULT_MISC = (((DEFAULT_FLICKER & 0x1) ^ 1) << 4) | ((DEFAULT_AUTOSTATE & 0x3) << 5) | (DEFAULT_AUTOSLEEP_TIME & 0x3); // https://github.com/Dwedit/PocketNES/blob/master/src/sram.c#L1045
 
 const GAME_TITLE = 'SAVE'; // No game title is listed in an NES ROM, so everything is just called this
 
@@ -62,6 +85,7 @@ export default class PocketNesEmulatorSaveData extends EmulatorBase {
     dataView.setUint16(POCKETNES_CONFIG_DATA_SIZE_OFFSET, POCKETNES_CONFIG_DATA_LENGTH, super.LITTLE_ENDIAN);
     dataView.setUint16(POCKETNES_CONFIG_DATA_TYPE_OFFSET, super.TYPE_CONFIG_DATA, super.LITTLE_ENDIAN);
     dataView.setUint8(POCKETNES_CONFIG_DATA_DISPLAY_TYPE_OFFSET, POCKETNES_CONFIG_DATA_DEFAULT_DISPLAY_TYPE);
+    dataView.setUint8(POCKETNES_CONFIG_DATA_MISC_OFFSET, POCKETNES_CONFIG_DATA_DEFAULT_MISC);
     dataView.setUint32(POCKETNES_CONFIG_DATA_SRAM_ROM_CHECKSUM_OFFSET, 0, super.LITTLE_ENDIAN); // Checksum here gets set to 0 so that the file is "clean"
 
     const encodedReservedData = textEncoder.encode(POCKETNES_CONFIG_DATA_RESERVED_DATA).slice(0, POCKETNES_CONFIG_DATA_RESERVED_LENGTH - 1);
