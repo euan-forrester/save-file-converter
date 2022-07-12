0bbf484852bf9aa83494edf328500efdfb62008d
Euan <euan_forrester@hotmail.com>
2022-07-11 20:13:59 -0700

    First attempt at making 2 dropdowns to select the game & flash cart types

--- frontend/src/components/FlashCartType.vue
+++ frontend/src/components/FlashCartType.vue
@@ -4,14 +4,31 @@
       <b-col cols=11 sm=12>
         <div>
           <b-form-select
-            v-bind:value="value"
-            v-on:input="$emit('input', $event)"
-            :options="options"
+            v-model="flashCartType"
+            v-on:input="changeFlashCartType()"
+            :options="flashCartTypes"
             :disabled="disabled"
           />
           <help-button
-            popover-text="Select the platform (Game Boy, Nintendo 64, etc.) that the save is for."
-            :id="this.id"
+            popover-text="Select the type of flash cart that you're using."
+            :id="`${this.id}-flash-cart-type`"
+            class="help-button"
+          />
+        </div>
+      </b-col>
+    </b-row>
+    <b-row no-gutters align-h="start" align-v="center">
+      <b-col cols=11 sm=12>
+        <div>
+          <b-form-select
+            v-model="platformType"
+            v-on:input="$emit('input', $event)"
+            :options="this.platformTypes[this.flashCartType]"
+            :disabled="disabled"
+          />
+          <help-button
+            popover-text="Select the type of game that the save is for."
+            :id="`${this.id}-platform-type`"
             class="help-button"
           />
         </div>
@@ -47,26 +64,66 @@ export default {
   components: {
     HelpButton,
   },
+  methods: {
+    changeFlashCartType() {
+      if (this.flashCartType !== null) {
+        if (this.platformTypes[this.flashCartType].length === 1) {
+          this.platformType = this.platformTypes[this.flashCartType][0].value;
+        } else {
+          this.platformType = null; // If > 1 item in the list, then display the "Choose game type" option
+        }
+      } else {
+        this.platformType = null;
+      }
+    },
+  },
   data() {
     return {
-      // Commented out ones are platforms we support elsewhere on the site but are currently available on the MiSTer
-
-      options: [
-        { value: null, text: 'Choose platform', disabled: true },
-        { value: 'FlashCart-NES', text: 'NES' },
-        { value: 'FlashCart-SNES', text: 'SNES' },
-        { value: 'FlashCart-GB', text: 'Gameboy/Gameboy Color' },
-        { value: 'FlashCart-GBA', text: 'Gameboy Advance' },
-        { value: 'FlashCart-GoombaEmulator', text: 'GB/GBC on GBA (Goomba emulator)' },
-        { value: 'FlashCart-PocketNesEmulator', text: 'NES on GBA (PocketNES emulator)' },
-        { value: 'FlashCart-SMSAdvanceEmulator', text: 'SMS on GBA (SMSAdvance emulator)' },
-        { value: 'FlashCart-GenesisEverdrive', text: 'Genesis (Everdrive)' },
-        { value: 'FlashCart-NESGenesisEverdrive', text: 'NES on Genesis (Everdrive)' },
-        { value: 'FlashCart-SMSGenesisEverdrive', text: 'SMS on Genesis (Everdrive)' },
-        { value: 'FlashCart-GenesisMegaSD', text: 'Genesis (Mega SD)' },
-        { value: 'FlashCart-SMSGenesisMegaSD', text: 'SMS on Genesis (Mega SD)' },
-        { value: 'FlashCart-N64', text: 'Nintendo 64' },
+      flashCartType: null,
+      platformType: null,
+      flashCartTypes: [
+        { value: null, text: 'Choose flash cart type', disabled: true },
+        { value: 'NES', text: 'NES' },
+        { value: 'SNES', text: 'SNES' },
+        { value: 'GB', text: 'Gameboy/Gameboy Color' },
+        { value: 'GBA', text: 'Gameboy Advance' },
+        { value: 'GenesisEverdrive', text: 'Genesis (Everdrive)' },
+        { value: 'GenesisMegaSD', text: 'Genesis (Mega SD)' },
+        { value: 'N64', text: 'Nintendo 64' },
       ],
+
+      platformTypes: {
+        NES: [
+          { value: 'FlashCart-NES', text: 'NES' },
+        ],
+        SNES: [
+          { value: 'FlashCart-SNES', text: 'SNES' },
+        ],
+        GB: [
+          { value: 'FlashCart-GB', text: 'Gameboy/Gameboy Color' },
+        ],
+        GBA: [
+          { value: null, text: 'Choose game type', disabled: true },
+          { value: 'FlashCart-GBA', text: 'Gameboy Advance' },
+          { value: 'FlashCart-GoombaEmulator', text: 'GB/GBC (Goomba emulator)' },
+          { value: 'FlashCart-PocketNesEmulator', text: 'NES (PocketNES emulator)' },
+          { value: 'FlashCart-SMSAdvanceEmulator', text: 'SMS (SMSAdvance emulator)' },
+        ],
+        GenesisEverdrive: [
+          { value: null, text: 'Choose game type', disabled: true },
+          { value: 'FlashCart-GenesisEverdrive', text: 'Genesis' },
+          { value: 'FlashCart-NESGenesisEverdrive', text: 'NES' },
+          { value: 'FlashCart-SMSGenesisEverdrive', text: 'SMS' },
+        ],
+        GenesisMegaSD: [
+          { value: null, text: 'Choose game type', disabled: true },
+          { value: 'FlashCart-GenesisMegaSD', text: 'Genesis' },
+          { value: 'FlashCart-SMSGenesisMegaSD', text: 'SMS' },
+        ],
+        N64: [
+          { value: 'FlashCart-N64', text: 'Nintendo 64' },
+        ],
+      },
     };
   },
 };
