85d84fd70b601c329e426f0c2b21de74801c99e2
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2022-07-19 14:25:45 -0700

    Split flash cart selection into 2 dropdowns (#145)
    * First attempt at making 2 dropdowns to select the game & flash cart types
    
    * Trying out adding labels
    
    * Remove labels again, but add descriptions to dropdown items
    
    * Made bottom dropdown disappear when it's not needed
    
    * Hide mega sd genesis output size because we always pad
    
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/components/FlashCartType.vue
+++ frontend/src/components/FlashCartType.vue
@@ -4,19 +4,38 @@
       <b-col cols=11 sm=12>
         <div>
           <b-form-select
-            v-bind:value="value"
-            v-on:input="$emit('input', $event)"
-            :options="options"
+            v-model="flashCartType"
+            v-on:input="changeFlashCartType()"
+            :options="flashCartTypes"
             :disabled="disabled"
           />
           <help-button
-            popover-text="Select the platform (Game Boy, Nintendo 64, etc.) that the save is for."
-            :id="this.id"
+            popover-text="Select the type of flash cart that you're using."
+            :id="`${this.id}-flash-cart-type`"
             class="help-button"
           />
         </div>
       </b-col>
     </b-row>
+    <div v-if="(this.flashCartType !== null) && (this.platformTypes[this.flashCartType].length > 1)">
+      <b-row no-gutters align-h="start" align-v="center">
+        <b-col cols=11 sm=12>
+          <div>
+            <b-form-select
+              v-model="platformType"
+              v-on:input="$emit('input', $event)"
+              :options="this.platformTypes[this.flashCartType]"
+              :disabled="disabled"
+            />
+            <help-button
+              popover-text="Select the type of game that the save is for."
+              :id="`${this.id}-platform-type`"
+              class="help-button"
+            />
+          </div>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
@@ -25,6 +44,10 @@
     position: absolute;
     right: -1.2em;
     top: 0em;
+  }
+
+  .text-label {
+    padding-left: 0.75em;
   }
 </style>
 
@@ -47,26 +70,67 @@ export default {
   components: {
     HelpButton,
   },
+  methods: {
+    changeFlashCartType() {
+      if (this.flashCartType !== null) {
+        if (this.platformTypes[this.flashCartType].length === 1) {
+          this.platformType = this.platformTypes[this.flashCartType][0].value;
+          this.$emit('input', this.platformType); // The second dropdown is invisible, so no events will be emitted from it because nothing will be selected
+        } else {
+          this.platformType = null; // If > 1 item in the list, then display the "Choose game type" option
+        }
+      } else {
+        this.platformType = null;
+      }
+    },
+  },
   data() {
     return {
-      // Commented out ones are platforms we support elsewhere on the site but are currently available on the MiSTer
-
-      options: [
-        { value: null, text: 'Choose platform', disabled: true },
-        { value: 'FlashCart-NES', text: 'NES' },
-        { value: 'FlashCart-SNES', text: 'SNES' },
-        { value: 'FlashCart-GB', text: 'Gameboy/Gameboy Color' },
-        { value: 'FlashCart-GBA', text: 'Gameboy Advance' },
-        { value: 'FlashCart-GoombaEmulator', text: 'GB/GBC on GBA (Goomba emulator)' },
-        { value: 'FlashCart-PocketNesEmulator', text: 'NES on GBA (PocketNES emulator)' },
-        { value: 'FlashCart-SMSAdvanceEmulator', text: 'SMS on GBA (SMSAdvance emulator)' },
-        { value: 'FlashCart-GenesisEverdrive', text: 'Genesis (Everdrive)' },
-        { value: 'FlashCart-NESGenesisEverdrive', text: 'NES on Genesis (Everdrive)' },
-        { value: 'FlashCart-SMSGenesisEverdrive', text: 'SMS on Genesis (Everdrive)' },
-        { value: 'FlashCart-GenesisMegaSD', text: 'Genesis (Mega SD)' },
-        { value: 'FlashCart-SMSGenesisMegaSD', text: 'SMS on Genesis (Mega SD)' },
-        { value: 'FlashCart-N64', text: 'Nintendo 64' },
+      flashCartType: null,
+      platformType: null,
+      flashCartTypes: [
+        { value: null, text: 'Choose flash cart type', disabled: true },
+        { value: 'NES', text: 'NES cart' },
+        { value: 'SNES', text: 'SNES cart' },
+        { value: 'GB', text: 'Gameboy/Gameboy Color cart' },
+        { value: 'GBA', text: 'Gameboy Advance cart' },
+        { value: 'GenesisEverdrive', text: 'Genesis cart (Everdrive)' },
+        { value: 'GenesisMegaSD', text: 'Genesis cart (Mega SD)' },
+        { value: 'N64', text: 'Nintendo 64 cart' },
       ],
+
+      platformTypes: {
+        NES: [
+          { value: 'FlashCart-NES', text: 'NES game' },
+        ],
+        SNES: [
+          { value: 'FlashCart-SNES', text: 'SNES game' },
+        ],
+        GB: [
+          { value: 'FlashCart-GB', text: 'Gameboy/Gameboy Color game' },
+        ],
+        GBA: [
+          { value: null, text: 'Choose game type', disabled: true },
+          { value: 'FlashCart-GBA', text: 'Gameboy Advance game' },
+          { value: 'FlashCart-GoombaEmulator', text: 'GB/GBC game (Goomba emulator)' },
+          { value: 'FlashCart-PocketNesEmulator', text: 'NES game (PocketNES emulator)' },
+          { value: 'FlashCart-SMSAdvanceEmulator', text: 'SMS game (SMSAdvance emulator)' },
+        ],
+        GenesisEverdrive: [
+          { value: null, text: 'Choose game type', disabled: true },
+          { value: 'FlashCart-GenesisEverdrive', text: 'Genesis game' },
+          { value: 'FlashCart-NESGenesisEverdrive', text: 'NES game' },
+          { value: 'FlashCart-SMSGenesisEverdrive', text: 'SMS game' },
+        ],
+        GenesisMegaSD: [
+          { value: null, text: 'Choose game type', disabled: true },
+          { value: 'FlashCart-GenesisMegaSD', text: 'Genesis game' },
+          { value: 'FlashCart-SMSGenesisMegaSD', text: 'SMS game' },
+        ],
+        N64: [
+          { value: 'FlashCart-N64', text: 'Nintendo 64 game' },
+        ],
+      },
     };
   },
 };
