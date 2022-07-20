438f9bd78d087cc4d8eff7c825e75ce395701a45
Euan <euan_forrester@hotmail.com>
2022-07-19 21:50:41 -0700

    Remove paypal tracking pixel (which 404s anyway)

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -21,7 +21,6 @@
       <form action="https://www.paypal.com/donate" method="post" target="_top">
         <input type="hidden" name="hosted_button_id" value="DHDERHCQVLGPJ" />
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
-        <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
       </form>
     </div>
     <br/>
