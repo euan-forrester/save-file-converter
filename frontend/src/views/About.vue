ed13f01f0729a97142e082d2c4b7f504f970a736
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2022-07-19 21:03:11 -0700

    Added button to donate via paypal (#146)
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -14,6 +14,18 @@
     </div>
     <br/>
     <div>
+      Everything on this site is free and open source with no advertising. If you find the site helpful and want to donate you can here:
+    </div>
+    <br/>
+    <div>
+      <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <input type="hidden" name="hosted_button_id" value="DHDERHCQVLGPJ" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+    </div>
+    <br/>
+    <div>
       Questions/comments/suggestions? savefileconverter{{'\xa0'}}(at){{'\xa0'}}gmail{{'\xa0'}}(dot){{'\xa0'}}com
     </div>
   </div>
