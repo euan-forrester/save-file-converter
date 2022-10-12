988cbae5d959f4b48ab8e3eeb72e496268aeb528
Euan <euan_forrester@hotmail.com>
2022-10-12 14:08:33 -0700

    Added tests for converting only a portion of the sega cd save data to mega sd format

--- frontend/tests/unit/save-formats/FlashCarts/Genesis/MegaSD/SegaCd.spec.js
+++ frontend/tests/unit/save-formats/FlashCarts/Genesis/MegaSD/SegaCd.spec.js
@@ -12,7 +12,9 @@ const RAW_RAM_CART_FILENAME = `${DIR}/megasd/Popful Mail (U)-ram-cart.brm`;
 
 const MEGA_SD_INTERNAL_MEMORY_ONLY_FILENAME = `${DIR}/megasd/Popful Mail (U)-internal-memory-only.SRM`;
 
-const MEGA_SD_CONVERTED_BACK_FILENAME = `${DIR}/megasd/Popful Mail (U)-converted-back.SRM`; // There are a few bits different in the 'signature' at the end of the RAM cart section, but both files should work
+const MEGA_SD_CONVERTED_BACK_INTERNAL_MEMORY_FILENAME = `${DIR}/megasd/Popful Mail (U)-converted-back-internal-memory.SRM`;
+const MEGA_SD_CONVERTED_BACK_RAM_CART_FILENAME = `${DIR}/megasd/Popful Mail (U)-converted-back-ram-cart.SRM`;
+const MEGA_SD_CONVERTED_BACK_BOTH_FILENAME = `${DIR}/megasd/Popful Mail (U)-converted-back.SRM`; // There are a few bits different in the 'signature' at the end of the RAM cart section, but both files should work
 
 describe('Flash cart - Genesis - Mega SD - Sega CD', () => {
   it('should convert a Mega SD Sega CD save to emulator format', async () => {
@@ -37,16 +39,31 @@ describe('Flash cart - Genesis - Mega SD - Sega CD', () => {
     expect(ArrayBufferUtil.arrayBuffersEqual(flashCartSaveData.getRawArrayBuffer(GenesisMegaSdSegaCdFlashCartSaveData.RAM_CART), rawRamCartSaveArrayBuffer)).to.equal(true);
   });
 
-  it('should convert emulator Sega CD files to Mega SD format', async () => {
+  it('should convert an emulator internal memory Sega CD file to Mega SD format', async () => {
+    const rawInternalSaveArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_INTERNAL_MEMORY_FILENAME);
+    const flashCartArrayBuffer = await ArrayBufferUtil.readArrayBuffer(MEGA_SD_CONVERTED_BACK_INTERNAL_MEMORY_FILENAME);
+
+    const flashCartSaveData = GenesisMegaSdSegaCdFlashCartSaveData.createFromRawData({ rawInternalSaveArrayBuffer });
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(flashCartSaveData.getFlashCartArrayBuffer(), flashCartArrayBuffer)).to.equal(true);
+  });
+
+  it('should convert an emulator RAM cart Sega CD file to Mega SD format', async () => {
+    const rawRamCartSaveArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_RAM_CART_FILENAME);
+    const flashCartArrayBuffer = await ArrayBufferUtil.readArrayBuffer(MEGA_SD_CONVERTED_BACK_RAM_CART_FILENAME);
+
+    const flashCartSaveData = GenesisMegaSdSegaCdFlashCartSaveData.createFromRawData({ rawRamCartSaveArrayBuffer });
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(flashCartSaveData.getFlashCartArrayBuffer(), flashCartArrayBuffer)).to.equal(true);
+  });
+
+  it('should convert emulator internal memory + RAM cart Sega CD files to Mega SD format', async () => {
     const rawInternalSaveArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_INTERNAL_MEMORY_FILENAME);
     const rawRamCartSaveArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_RAM_CART_FILENAME);
-    const flashCartArrayBuffer = await ArrayBufferUtil.readArrayBuffer(MEGA_SD_CONVERTED_BACK_FILENAME);
+    const flashCartArrayBuffer = await ArrayBufferUtil.readArrayBuffer(MEGA_SD_CONVERTED_BACK_BOTH_FILENAME);
 
     const flashCartSaveData = GenesisMegaSdSegaCdFlashCartSaveData.createFromRawData({ rawInternalSaveArrayBuffer, rawRamCartSaveArrayBuffer });
 
     expect(ArrayBufferUtil.arrayBuffersEqual(flashCartSaveData.getFlashCartArrayBuffer(), flashCartArrayBuffer)).to.equal(true);
   });
-
-  // FIXME: Add test for making mega sd file with only internal memory
-  // FIXME: Add test for making mega sd file with only ram cart
 });
