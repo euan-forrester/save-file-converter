4e376a164bbe72f425c024d64790b2fde6da847a
Euan <euan_forrester@hotmail.com>
2023-01-01 00:51:21 -0800

    Reworked the columns to look prettier

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,48 +1,65 @@
 <template>
   <div class="about">
 
-    <b-row class="justify-content-md-center header-row">
-      <h2>Join us on Discord</h2>
-    </b-row>
-
-    <b-row class="justify-content-md-center">
-      <b-col cols="3">
-        <b-link href="https://discord.gg/wtJ7xUKKTR">
-          <b-img src="discord-channels.png"/>
-        </b-link>
-        <b-link href="https://discord.gg/wtJ7xUKKTR">https://discord.gg/wtJ7xUKKTR</b-link>
+    <b-row class="justify-content-center">
+      <b-col cols="11" md="5">
+        <b-row class="justify-content-center">
+          <h2>Join us on Discord</h2>
+        </b-row>
+        <b-row class="justify-content-center">
+          <b-col>
+            <b-row class="justify-content-center">
+              <b-link href="https://discord.gg/wtJ7xUKKTR">
+                <b-img src="discord-channels.png"/>
+              </b-link>
+            </b-row>
+            <b-row class="justify-content-center">
+              <b-link href="https://discord.gg/wtJ7xUKKTR">
+                https://discord.gg/wtJ7xUKKTR
+              </b-link>
+            </b-row>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
 
-    <b-row class="justify-content-md-center header-row">
-      <h2>Contact by email</h2>
-    </b-row>
-    <b-row class="justify-content-md-center">
-      savefileconverter{{'\xa0'}}(at){{'\xa0'}}gmail{{'\xa0'}}(dot){{'\xa0'}}com
-    </b-row>
+    <b-row class="justify-content-center">
 
-    <b-row class="justify-content-md-center header-row">
-      <h2>Donate</h2>
-    </b-row>
-    <b-row class="justify-content-md-center">
-      Everything on this site is free and open source with no advertising. If you find the site helpful and want to donate you can here:
-    </b-row>
-    <b-row class="justify-content-md-center">
-      <div>
-        <form action="https://www.paypal.com/donate" method="post" target="_top">
-          <input type="hidden" name="hosted_button_id" value="DHDERHCQVLGPJ" />
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
-        </form>
-      </div>
-    </b-row>
+      <b-col cols="11" md="4">
+        <b-row class="justify-content-center header-row">
+          <h2>Contact by email</h2>
+        </b-row>
+        <b-row class="justify-content-center info-row">
+          savefileconverter{{'\xa0'}}(at) gmail{{'\xa0'}}(dot){{'\xa0'}}com
+        </b-row>
+      </b-col>
 
-    <b-row class="justify-content-md-center header-row">
-      <h2>Contribute code</h2>
-    </b-row>
-    <b-row class="justify-content-md-center">
-      <b-link href="https://github.com/euan-forrester/save-file-converter">
-        https://github.com/euan-forrester/save-file-converter
-      </b-link>
+      <b-col cols="11" md="3">
+        <b-row class="justify-content-center header-row">
+          <h2>Donate</h2>
+        </b-row>
+        <b-row class="justify-content-center info-row">
+          Everything on this site is free and open source with no advertising. If you find the site helpful and want to donate you can here:
+          <div class="donate-button">
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+              <input type="hidden" name="hosted_button_id" value="DHDERHCQVLGPJ" />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
+            </form>
+          </div>
+        </b-row>
+      </b-col>
+
+      <b-col cols="11" md="4">
+        <b-row class="justify-content-center header-row">
+          <h2>Contribute code</h2>
+        </b-row>
+        <b-row class="justify-content-center info-row">
+          <b-link href="https://github.com/euan-forrester/save-file-converter">
+            https://github.com/euan-forrester/save-file-converter
+          </b-link>
+        </b-row>
+      </b-col>
+
     </b-row>
 
   </div>
@@ -50,6 +67,15 @@
 
 <style scoped>
 .header-row {
+  padding-top: 2em;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+}
+.info-row {
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+}
+.donate-button {
   padding-top: 1em;
 }
 </style>
