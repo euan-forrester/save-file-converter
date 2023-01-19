393c2f5d1db9e07d8e77a9eadc8d4c5582b13f77
Euan <euan_forrester@hotmail.com>
2023-01-19 14:21:27 -0800

    Added test for larger files that have encoded data

--- frontend/tests/unit/save-formats/SegaCd/SegaCd.spec.js
+++ frontend/tests/unit/save-formats/SegaCd/SegaCd.spec.js
@@ -20,6 +20,14 @@ const INTERNAL_MEMORY_MULTIPLE_FILES_FILES = [
   `${DIR}/Multiple titles-7.srm`,
 ];
 
+const RAM_CARTRIDGE_ENCODED_DATA_FILENAME = `${DIR}/SHINING FORCE CD.brm`;
+const RAM_CARTRIDGE_ENCODED_DATA_FILES = [
+  `${DIR}/SHINING FORCE CD-1.srm`,
+  `${DIR}/SHINING FORCE CD-2.srm`,
+  `${DIR}/SHINING FORCE CD-3.srm`,
+  `${DIR}/SHINING FORCE CD-4.srm`,
+];
+
 describe('Sega CD', () => {
   it('should extract a save from an internal memory file containing 1 save', async () => {
     const segaCdArrayBuffer = await ArrayBufferUtil.readArrayBuffer(INTERNAL_MEMORY_1_FILE_FILENAME);
@@ -95,5 +103,43 @@ describe('Sega CD', () => {
     expect(segaCdSaveData.getSaveFiles()[6].startBlockNumber).to.equal(88);
     expect(segaCdSaveData.getSaveFiles()[6].fileSizeBlocks).to.equal(11);
     expect(ArrayBufferUtil.arrayBuffersEqual(segaCdSaveData.getSaveFiles()[6].fileData, rawArrayBuffers[6])).to.equal(true);
+  });
+
+  it('should extract all of the saves from a RAM cartridge file containing encoded data', async () => {
+    const segaCdArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAM_CARTRIDGE_ENCODED_DATA_FILENAME);
+    const rawArrayBuffers = await Promise.all(RAM_CARTRIDGE_ENCODED_DATA_FILES.map((n) => ArrayBufferUtil.readArrayBuffer(n)));
+
+    const segaCdSaveData = SegaCdSaveData.createFromSegaCdData(segaCdArrayBuffer);
+
+    expect(segaCdSaveData.getNumFreeBlocks()).to.equal(623);
+    expect(segaCdSaveData.getFormat()).to.equal('SEGA_CD_ROM');
+    expect(segaCdSaveData.getVolume()).to.equal('');
+    expect(segaCdSaveData.getMediaId()).to.equal('RAM_CARTRIDGE');
+
+    expect(segaCdSaveData.getSaveFiles().length).to.equal(4);
+
+    expect(segaCdSaveData.getSaveFiles()[0].filename).to.equal('SFCD_DAT_01');
+    expect(segaCdSaveData.getSaveFiles()[0].dataIsEncoded).to.equal(true);
+    expect(segaCdSaveData.getSaveFiles()[0].startBlockNumber).to.equal(1);
+    expect(segaCdSaveData.getSaveFiles()[0].fileSizeBlocks).to.equal(99);
+    expect(ArrayBufferUtil.arrayBuffersEqual(segaCdSaveData.getSaveFiles()[0].fileData, rawArrayBuffers[0])).to.equal(true);
+
+    expect(segaCdSaveData.getSaveFiles()[1].filename).to.equal('SFCD_DAT_02');
+    expect(segaCdSaveData.getSaveFiles()[1].dataIsEncoded).to.equal(true);
+    expect(segaCdSaveData.getSaveFiles()[1].startBlockNumber).to.equal(100);
+    expect(segaCdSaveData.getSaveFiles()[1].fileSizeBlocks).to.equal(99);
+    expect(ArrayBufferUtil.arrayBuffersEqual(segaCdSaveData.getSaveFiles()[1].fileData, rawArrayBuffers[1])).to.equal(true);
+
+    expect(segaCdSaveData.getSaveFiles()[2].filename).to.equal('SFCD_DAT_03');
+    expect(segaCdSaveData.getSaveFiles()[2].dataIsEncoded).to.equal(true);
+    expect(segaCdSaveData.getSaveFiles()[2].startBlockNumber).to.equal(199);
+    expect(segaCdSaveData.getSaveFiles()[2].fileSizeBlocks).to.equal(99);
+    expect(ArrayBufferUtil.arrayBuffersEqual(segaCdSaveData.getSaveFiles()[2].fileData, rawArrayBuffers[2])).to.equal(true);
+
+    expect(segaCdSaveData.getSaveFiles()[3].filename).to.equal('SFCD_DAT_04');
+    expect(segaCdSaveData.getSaveFiles()[3].dataIsEncoded).to.equal(true);
+    expect(segaCdSaveData.getSaveFiles()[3].startBlockNumber).to.equal(298);
+    expect(segaCdSaveData.getSaveFiles()[3].fileSizeBlocks).to.equal(99);
+    expect(ArrayBufferUtil.arrayBuffersEqual(segaCdSaveData.getSaveFiles()[3].fileData, rawArrayBuffers[3])).to.equal(true);
   });
 });
