f19b57742b425da02b24344bbd70e0974268606c
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2023-03-17 21:11:31 -0700

    Fix for NSO GB saves that use a newer version number (#222)
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/save-formats/NintendoSwitchOnline/Gameboy.js
+++ frontend/src/save-formats/NintendoSwitchOnline/Gameboy.js
@@ -6,9 +6,13 @@ The format is:
 
 0x00: Magic: "SRAM" + 0x03
 0x08: Encoded SHA-1 hash of the ROM
-0x30: Magic: 0x0B000000 + "HEAD-v178.0"
+0x30: Magic: 0x0B000000 + "HEAD-vXXX.X"
 0x40: Encoded SHA-1 hash of the raw save data
 0x68: Raw save data
+
+It appears there's at least 2 versions of the HEAD magic:
+- HEAD-v178.0 (the initial batch of NSO games)
+- HEAD-v184.0 (Kirby's Dreamland 2)
 */
 
 import SaveFilesUtil from '../../util/SaveFiles';
@@ -18,8 +22,10 @@ import HashUtil from '../../util/Hash';
 const MAGIC1 = [0x53, 0x52, 0x41, 0x4D, 0x03]; // 'SRAM';
 const MAGIC1_OFFSET = 0;
 
-const MAGIC2 = [0x0B, 0x00, 0x00, 0x00, 0x48, 0x45, 0x41, 0x44, 0x2D, 0x76, 0x31, 0x37, 0x38, 0x2E, 0x30]; // 'HEAD-v178.0';
+const MAGIC2 = [0x0B, 0x00, 0x00, 0x00, 0x48, 0x45, 0x41, 0x44, 0x2D, 0x76]; // 'HEAD-v';
 const MAGIC2_OFFSET = 0x30;
+const VERSION_NUMBER_OFFSET = MAGIC2_OFFSET + MAGIC2.length;
+const VERSION_NUMBER_LENGTH = 5;
 
 const ROM_HASH_OFFSET = 0x08;
 const SAVE_DATA_HASH_OFFSET = 0x40;
@@ -60,15 +66,17 @@ export default class NsoGameboySaveData {
     Util.checkMagicBytes(nsoArrayBuffer, MAGIC2_OFFSET, MAGIC2);
 
     const encodedRomHashArrayBuffer = nsoArrayBuffer.slice(ROM_HASH_OFFSET, ROM_HASH_OFFSET + HASH_LENGTH);
+    const encodedVersionArrayBuffer = nsoArrayBuffer.slice(VERSION_NUMBER_OFFSET, VERSION_NUMBER_OFFSET + VERSION_NUMBER_LENGTH);
 
-    return new NsoGameboySaveData(nsoArrayBuffer.slice(DATA_BEGIN_OFFSET), nsoArrayBuffer, encodedRomHashArrayBuffer);
+    return new NsoGameboySaveData(nsoArrayBuffer.slice(DATA_BEGIN_OFFSET), nsoArrayBuffer, encodedRomHashArrayBuffer, encodedVersionArrayBuffer);
   }
 
-  static createFromRawData(rawArrayBuffer, encodedRomHashArrayBuffer) {
+  static createFromRawData(rawArrayBuffer, encodedRomHashArrayBuffer, encodedVersionArrayBuffer) {
     let headerArrayBuffer = Util.getFilledArrayBuffer(HEADER_LENGTH, HEADER_FILL_VALUE);
 
     headerArrayBuffer = Util.setMagicBytes(headerArrayBuffer, MAGIC1_OFFSET, MAGIC1);
     headerArrayBuffer = Util.setMagicBytes(headerArrayBuffer, MAGIC2_OFFSET, MAGIC2);
+    headerArrayBuffer = Util.setArrayBufferPortion(headerArrayBuffer, encodedVersionArrayBuffer, VERSION_NUMBER_OFFSET, 0, VERSION_NUMBER_LENGTH);
 
     const encodedSaveDataHashArrayBuffer = HashUtil.getEncodedHash(rawArrayBuffer, HASH_ALGORITHM, HASH_ENCODING);
 
@@ -77,14 +85,15 @@ export default class NsoGameboySaveData {
 
     const nsoArrayBuffer = Util.concatArrayBuffers([headerArrayBuffer, rawArrayBuffer]);
 
-    return new NsoGameboySaveData(rawArrayBuffer, nsoArrayBuffer, encodedRomHashArrayBuffer);
+    return new NsoGameboySaveData(rawArrayBuffer, nsoArrayBuffer, encodedRomHashArrayBuffer, encodedVersionArrayBuffer);
   }
 
   // This constructor creates a new object from a binary representation of a NSO save data file
-  constructor(rawArrayBuffer, nsoArrayBuffer, encodedRomHash) {
+  constructor(rawArrayBuffer, nsoArrayBuffer, encodedRomHash, encodedVersion) {
     this.rawArrayBuffer = rawArrayBuffer;
     this.nsoArrayBuffer = nsoArrayBuffer;
     this.encodedRomHash = encodedRomHash;
+    this.encodedVersion = encodedVersion;
   }
 
   getRawArrayBuffer() {
@@ -97,5 +106,9 @@ export default class NsoGameboySaveData {
 
   getEncodedRomHash() {
     return this.encodedRomHash;
+  }
+
+  getEncodedVersion() {
+    return this.encodedVersion;
   }
 }
