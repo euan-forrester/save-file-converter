99ac02f12d89cdad9668dcc1c710d9d1034b2bff
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2023-07-02 14:20:53 -0700

    Remove the New! tags from features that are no longer new (#248)
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,7 +14,7 @@
       </b-row>
       <b-row>
         <b-col class="nav-row">
-          <router-link to="/nintendo-switch-online">Nintendo{{'\xa0'}}Switch{{'\xa0'}}Online{{'\xa0'}}<span class="new-text">(New!)</span></router-link> |
+          <router-link to="/nintendo-switch-online">Nintendo{{'\xa0'}}Switch{{'\xa0'}}Online{{'\xa0'}}</router-link> |
           <router-link to="/wii">Wii{{'\xa0'}}Virtual{{'\xa0'}}Console</router-link>
         </b-col>
       </b-row>
@@ -41,7 +41,7 @@
       </b-row>
       <b-row>
         <b-col class="nav-row">
-          <router-link to="/sega-cd">Sega{{'\xa0'}}CD{{'\xa0'}}<span class="new-text">(New!)</span></router-link> |
+          <router-link to="/sega-cd">Sega{{'\xa0'}}CD{{'\xa0'}}</router-link> |
           <router-link to="/psp">PSP{{'\xa0'}}-{{'\xa0'}}Save{{'\xa0'}}decryption{{'\xa0'}}</router-link>
         </b-col>
       </b-row>
