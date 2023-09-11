0cb7e4c6c97a132e780b7433e204a443725af951
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2023-09-10 20:14:16 -0700

    Add support for new Switch Gameboy file format (#260)
    * Fixed error in front end when trying to resize switch gameboy save
    
    * Successfully convert Switch Gameboy format C games to raw
    
    * Successfully turn a raw Gameboy save into a Switch Gameboy one
    
    ---------
    
    Co-authored-by: Euan <euan_forrester@hotmail.com>

--- frontend/src/components/ConvertNintendoSwitchOnline.vue
+++ frontend/src/components/ConvertNintendoSwitchOnline.vue
@@ -256,7 +256,8 @@ export default {
                   this.inputArrayBuffer,
                   this.exampleNsoData.getEncodedRomHash(),
                   this.exampleNsoData.getEncodedVersion(),
-                  this.exampleNsoData.getMagic2Type(),
+                  this.exampleNsoData.getUnknownData(),
+                  this.exampleNsoData.getFileFormat(),
                 );
                 this.outputFilesize = this.nsoSaveData.getRawArrayBuffer().byteLength;
               }
