ec5a8edb1363ba04042e23672c625f99406c2622
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2024-02-23 13:24:59 -0800

    Setting up the scaffolding for the new screen

---
+++ frontend/src/components/AdvancedUtils.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <b-container>
+      <div class="text-center">
+        <b-button @click="tabIndex--">Previous</b-button>
+        <b-button @click="tabIndex++">Next</b-button>
+
+        <div class="text-muted">Current Tab: {{ tabIndex }}</div>
+      </div>
+
+      <b-tabs v-model="tabIndex" content-class="mt-3" justified>
+        <b-tab title="Endianness" active>
+          <p>Endianness</p>
+          <endianness-word-size
+            model="endiannessSize"
+          />
+        </b-tab>
+        <b-tab title="Compression"><p>Compression</p></b-tab>
+        <b-tab :title="'Byte\xa0expansion'"><p>Byte&nbsp;expansion</p></b-tab>
+        <b-tab title="Slice"><p>Slice</p></b-tab>
+        <b-tab title="Resize"><p>Resize</p></b-tab>
+        <b-tab title="Header/footer"><p>Add/remove header/footer</p></b-tab>
+
+      </b-tabs>
+
+      <b-row no-gutters align-h="center" align-v="start">
+        <b-col sm=12 md=5 align-self="center">
+          <b-row no-gutters align-h="center" align-v="start">
+            <b-col cols=12>
+              <b-jumbotron fluid :header-level="$mq | mq({ xs: 5, sm: 5, md: 5, lg: 5, xl: 4 })">
+                <template v-slot:header>Erase save</template>
+              </b-jumbotron>
+            </b-col>
+          </b-row>
+          <input-file
+            @load="readSaveData($event)"
+            :errorMessage="this.errorMessage"
+            placeholderText="Choose a file to convert"
+            :leaveRoomForHelpIcon="false"
+          />
+        </b-col>
+      </b-row>
+      <b-row class="justify-content-md-center" align-h="center">
+        <b-col cols="auto" sm=4 md=3 lg=2 align-self="center">
+          <b-button
+            class="utilities-advanced-save-convert-button"
+            variant="success"
+            block
+            :disabled="!this.saveData || !outputFilename"
+            @click="convertFile()"
+          >
+          Convert!
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style scoped>
+
+/* Separate class for each different button to enable tracking in google tag manager */
+.utilities-advanced-convert-button {
+  margin-top: 1em;
+}
+
+.help {
+  margin-top: 1em;
+}
+</style>
+
+<script>
+import { saveAs } from 'file-saver';
+import SaveFilesUtil from '../util/SaveFiles';
+import Util from '../util/util';
+import InputFile from './InputFile.vue';
+import EndiannessWordSize from './EndiannessWordSize.vue';
+
+export default {
+  name: 'AdvancedUtils',
+  data() {
+    return {
+      saveData: null,
+      errorMessage: null,
+      outputFilename: null,
+      endiannessSize: null,
+      tabIndex: 1,
+    };
+  },
+  /*
+  mounted() {
+    // Need to keep these in sync with the template above. Is there a way to get these programmatically?
+    const possibleTabNames = [
+      'endianness',
+      'compression',
+      'byte-expansion',
+      'slice',
+      'resize',
+      'header-footer',
+    ];
+
+    const initialTabIndex = possibleTabNames.indexOf(this.initialTab);
+
+    console.log(`initlalTab: '${this.initialTab}', index: ${initialTabIndex}`);
+
+    this.tabIndex = (initialTabIndex >= 0) ? initialTabIndex : 0;
+  },
+  */
+  components: {
+    InputFile,
+    EndiannessWordSize,
+  },
+  props: {
+    initialTab: {
+      type: String,
+      default: 'endianness',
+    },
+  },
+  methods: {
+    readSaveData(event) {
+      this.errorMessage = null;
+      this.outputFilename = null;
+      try {
+        this.saveData = event.arrayBuffer;
+        this.outputFilename = Util.getFilename(event.filename);
+      } catch (e) {
+        this.errorMessage = e.message;
+        this.retron5SaveData = null;
+      }
+    },
+    convertFile() {
+      const outputArrayBuffer = SaveFilesUtil.getEraseCartridgeSave(this.saveData);
+
+      const outputBlob = new Blob([outputArrayBuffer], { type: 'application/octet-stream' });
+
+      saveAs(outputBlob, this.outputFilename); // Frustratingly, in Firefox the dialog says "from: blob:" and apparently this can't be changed: https://github.com/eligrey/FileSaver.js/issues/101
+    },
+  },
+};
+
+</script>
---
+++ frontend/src/views/AdvancedView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <AdvancedUtils
+      :initialTab="this.initialTab"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AdvancedUtils from '@/components/AdvancedUtils.vue';
+
+export default {
+  name: 'AdvancedView',
+  components: {
+    AdvancedUtils,
+  },
+  props: {
+    initialTab: {
+      type: String,
+      default: 'endianness',
+    },
+  },
+};
+</script>
