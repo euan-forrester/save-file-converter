6a6e6ec9ccc4ab948e1349ab84416c6fd48d9257
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2024-04-16 13:43:08 -0700

    Rewrote NSO GB/GBC parsing to allow for git revision numbers of arbitrary length. Removed the concept of different "file formats"

--- frontend/tests/unit/save-formats/NintendoSwitchOnline/Gameboy.spec.js
+++ frontend/tests/unit/save-formats/NintendoSwitchOnline/Gameboy.spec.js
@@ -11,68 +11,67 @@ const DIR = './tests/data/save-formats/nintendoswitchonline/gb';
 const NSO_GB_FILENAME = `${DIR}/Metroid_II_Return_of_Samus.sram`;
 const RAW_GB_FILENAME = `${DIR}/Metroid_II_Return_of_Samus.sav`;
 const GB_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('74a2fad86b9a4c013149b1e214bc4600efb1066d'));
-const GB_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('178.0'));
-const GB_UNKNOWN_DATA = null;
-const GB_FILE_FORMAT = 'A';
-
-const NSO_GB_GBC_FILENAME = `${DIR}/Links_Awakening_DX.sram`;
-const RAW_GB_GBC_FILENAME = `${DIR}/Links_Awakening_DX.sav`;
-const GB_GBC_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('1c091225688d966928cc74336dbef2e07d12a47c'));
-const GB_GBC_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('178.0'));
-const GB_GBC_UNKNOWN_DATA = null;
-const GB_GBC_FILE_FORMAT = 'A';
-
-const NSO_GBC_FILENAME = `${DIR}/Wario_Land_3.sram`;
-const RAW_GBC_FILENAME = `${DIR}/Wario_Land_3.sav`;
-const GBC_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('bb7877309834441fd03adb7fa65738e5d5b2d7ba'));
-const GBC_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('178.0'));
-const GBC_UNKNOWN_DATA = null;
-const GBC_FILE_FORMAT = 'A';
+const GB_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('HEAD-v178.0'));
+const GB_RTC_DATA = null;
 
 const NSO_GB_NEWER_VERSION_FILENAME = `${DIR}/Kirbys_Dreamland_2.sram`;
 const RAW_GB_NEWER_VERSION_FILENAME = `${DIR}/Kirbys_Dreamland_2.sav`;
 const GB_NEWER_VERSION_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('8a2898ffa17e25f43793f40c88421d840d372d3c'));
-const GB_NEWER_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('184.0'));
-const GB_NEWER_VERSION_UNKNOWN_DATA = null;
-const GB_NEWER_VERSION_FILE_FORMAT = 'A';
+const GB_NEWER_VERSION_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('HEAD-v184.0'));
+const GB_NEWER_VERSION_RTC_DATA = null;
 
 const NSO_GBC_FORMAT_B_FILENAME = `${DIR}/Pokemon Cristal.sram`;
 const RAW_GBC_FORMAT_B_FILENAME = `${DIR}/Pokemon Cristal.sav`;
 const GBC_FORMAT_B_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('889a06fc0bb863666865aa69def0adf97945ac2a'));
-const GBC_FORMAT_B_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('203.0'));
-const GBC_FORMAT_B_UNKNOWN_DATA = Util.bufferToArrayBuffer(
+const GBC_FORMAT_B_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('HEAD-v203.0'));
+const GBC_FORMAT_B_RTC_DATA = Util.bufferToArrayBuffer(
   new Uint8Array(
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x0A, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00,
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x0A, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00],
   ),
 );
-const GB_FORMAT_B_FILE_FORMAT = 'B';
 
 const NSO_GBC_FORMAT_C_FILENAME = `${DIR}/Pokemon_TCG.sram`;
 const RAW_GBC_FORMAT_C_FILENAME = `${DIR}/Pokemon_TCG.sav`;
 const GBC_FORMAT_C_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('0f8670a583255cff3e5b7ca71b5d7454d928fc48'));
-const GBC_FORMAT_C_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('196.0'));
-const GBC_FORMAT_C_UNKNOWN_DATA = null;
-const GB_FORMAT_C_FILE_FORMAT = 'C';
+const GBC_FORMAT_C_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('master-v196.0'));
+const GBC_FORMAT_C_RTC_DATA = null;
+
+const NSO_GBC_FORMAT_C_WITH_COMMIT_HASH_FILENAME = `${DIR}/Pokemon_TCG_Europe.sram`;
+const RAW_GBC_FORMAT_C_WITH_COMMIT_HASH_FILENAME = `${DIR}/Pokemon_TCG_Europe.sav`;
+const GBC_FORMAT_C_WITH_COMMIT_HASH_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('dffc15f3063a4c2df84c6361406b41aec1696d3e'));
+const GBC_FORMAT_C_WITH_COMMIT_HASH_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('HEAD-v213.0-1-g59d2e63b'));
+const GBC_FORMAT_C_WITH_COMMIT_HASH_RTC_DATA = null;
 
 const NSO_GBC_FORMAT_D_FILENAME = `${DIR}/Pokemon_-_Crystal_Version.sram`;
 const RAW_GBC_FORMAT_D_FILENAME = `${DIR}/Pokemon_-_Crystal_Version.sav`;
 const GBC_FORMAT_D_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('f2f52230b536214ef7c9924f483392993e226cfb'));
-const GBC_FORMAT_D_VERSION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('199.0'));
-const GBC_FORMAT_D_UNKNOWN_DATA = Util.bufferToArrayBuffer(
+const GBC_FORMAT_D_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('master-v199.0'));
+const GBC_FORMAT_D_RTC_DATA = Util.bufferToArrayBuffer(
   new Uint8Array(
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
   ),
 );
-const GB_FORMAT_D_FILE_FORMAT = 'D';
+
+const NSO_GB_GBC_FILENAME = `${DIR}/Links_Awakening_DX.sram`;
+const RAW_GB_GBC_FILENAME = `${DIR}/Links_Awakening_DX.sav`;
+const GB_GBC_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('1c091225688d966928cc74336dbef2e07d12a47c'));
+const GB_GBC_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('HEAD-v178.0'));
+const GB_GBC_RTC_DATA = null;
+
+const NSO_GBC_FILENAME = `${DIR}/Wario_Land_3.sram`;
+const RAW_GBC_FILENAME = `${DIR}/Wario_Land_3.sav`;
+const GBC_ROM_HASH = Util.bufferToArrayBuffer(textEncoder.encode('bb7877309834441fd03adb7fa65738e5d5b2d7ba'));
+const GBC_GIT_REVISION_NUMBER = Util.bufferToArrayBuffer(textEncoder.encode('HEAD-v178.0'));
+const GBC_RTC_DATA = null;
 
 describe('Nintendo Switch Online - Gameboy', () => {
   it('should convert a raw GB save to NSO format', async () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GB_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GB_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GB_ROM_HASH, GB_VERSION_NUMBER, GB_UNKNOWN_DATA, GB_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GB_ROM_HASH, GB_GIT_REVISION_NUMBER, GB_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -83,9 +82,9 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GB_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GB_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GB_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GB_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GB_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(nsoSaveData.getRtcDataArrayBuffer()).to.equal(GB_RTC_DATA);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
@@ -93,7 +92,7 @@ describe('Nintendo Switch Online - Gameboy', () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GB_NEWER_VERSION_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GB_NEWER_VERSION_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GB_NEWER_VERSION_ROM_HASH, GB_NEWER_VERSION_NUMBER, GB_NEWER_VERSION_UNKNOWN_DATA, GB_NEWER_VERSION_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GB_NEWER_VERSION_ROM_HASH, GB_NEWER_VERSION_GIT_REVISION_NUMBER, GB_NEWER_VERSION_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -104,9 +103,9 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GB_NEWER_VERSION_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GB_NEWER_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GB_NEWER_VERSION_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GB_NEWER_VERSION_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GB_NEWER_VERSION_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(nsoSaveData.getRtcDataArrayBuffer()).to.equal(GB_NEWER_VERSION_RTC_DATA);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
@@ -114,7 +113,7 @@ describe('Nintendo Switch Online - Gameboy', () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GBC_FORMAT_B_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GBC_FORMAT_B_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_FORMAT_B_ROM_HASH, GBC_FORMAT_B_VERSION_NUMBER, GBC_FORMAT_B_UNKNOWN_DATA, GB_FORMAT_B_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_FORMAT_B_ROM_HASH, GBC_FORMAT_B_GIT_REVISION_NUMBER, GBC_FORMAT_B_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -125,10 +124,9 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GBC_FORMAT_B_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GBC_FORMAT_B_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getUnknownData(), GBC_FORMAT_B_UNKNOWN_DATA)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GB_FORMAT_B_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GBC_FORMAT_B_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GBC_FORMAT_B_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRtcDataArrayBuffer(), GBC_FORMAT_B_RTC_DATA)).to.equal(true);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
@@ -136,7 +134,7 @@ describe('Nintendo Switch Online - Gameboy', () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GBC_FORMAT_C_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GBC_FORMAT_C_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_FORMAT_C_ROM_HASH, GBC_FORMAT_C_VERSION_NUMBER, GBC_FORMAT_C_UNKNOWN_DATA, GB_FORMAT_C_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_FORMAT_C_ROM_HASH, GBC_FORMAT_C_GIT_REVISION_NUMBER, GBC_FORMAT_C_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -147,9 +145,35 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GBC_FORMAT_C_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GBC_FORMAT_C_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GB_FORMAT_C_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GBC_FORMAT_C_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GBC_FORMAT_C_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(nsoSaveData.getRtcDataArrayBuffer()).to.equal(GBC_FORMAT_C_RTC_DATA);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
+  });
+
+  it('should convert a raw format C GBC save with a commit hash to NSO format', async () => {
+    const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GBC_FORMAT_C_WITH_COMMIT_HASH_FILENAME);
+    const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GBC_FORMAT_C_WITH_COMMIT_HASH_FILENAME);
+
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(
+      rawArrayBuffer,
+      GBC_FORMAT_C_WITH_COMMIT_HASH_ROM_HASH,
+      GBC_FORMAT_C_WITH_COMMIT_HASH_GIT_REVISION_NUMBER,
+      GBC_FORMAT_C_WITH_COMMIT_HASH_RTC_DATA,
+    );
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
+  });
+
+  it('should convert a NSO format C GBC save with a commit hash to raw format', async () => {
+    const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GBC_FORMAT_C_WITH_COMMIT_HASH_FILENAME);
+    const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GBC_FORMAT_C_WITH_COMMIT_HASH_FILENAME);
+
+    const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GBC_FORMAT_C_WITH_COMMIT_HASH_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GBC_FORMAT_C_WITH_COMMIT_HASH_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(nsoSaveData.getRtcDataArrayBuffer()).to.equal(GBC_FORMAT_C_WITH_COMMIT_HASH_RTC_DATA);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
@@ -157,7 +181,7 @@ describe('Nintendo Switch Online - Gameboy', () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GBC_FORMAT_D_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GBC_FORMAT_D_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_FORMAT_D_ROM_HASH, GBC_FORMAT_D_VERSION_NUMBER, GBC_FORMAT_D_UNKNOWN_DATA, GB_FORMAT_D_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_FORMAT_D_ROM_HASH, GBC_FORMAT_D_GIT_REVISION_NUMBER, GBC_FORMAT_D_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -168,10 +192,9 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GBC_FORMAT_D_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GBC_FORMAT_D_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getUnknownData(), GBC_FORMAT_D_UNKNOWN_DATA)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GB_FORMAT_D_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GBC_FORMAT_D_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GBC_FORMAT_D_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRtcDataArrayBuffer(), GBC_FORMAT_D_RTC_DATA)).to.equal(true);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
@@ -179,7 +202,7 @@ describe('Nintendo Switch Online - Gameboy', () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GB_GBC_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GB_GBC_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GB_GBC_ROM_HASH, GB_GBC_VERSION_NUMBER, GB_GBC_UNKNOWN_DATA, GB_GBC_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GB_GBC_ROM_HASH, GB_GBC_GIT_REVISION_NUMBER, GB_GBC_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -190,9 +213,9 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GB_GBC_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GB_GBC_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GB_GBC_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GB_GBC_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GB_GBC_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(nsoSaveData.getRtcDataArrayBuffer()).to.equal(GB_GBC_RTC_DATA);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 
@@ -200,7 +223,7 @@ describe('Nintendo Switch Online - Gameboy', () => {
     const nsoArrayBuffer = await ArrayBufferUtil.readArrayBuffer(NSO_GBC_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_GBC_FILENAME);
 
-    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_ROM_HASH, GBC_VERSION_NUMBER, GBC_UNKNOWN_DATA, GBC_FILE_FORMAT);
+    const nsoSaveData = NsoGameboySaveData.createFromRawData(rawArrayBuffer, GBC_ROM_HASH, GBC_GIT_REVISION_NUMBER, GBC_RTC_DATA);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getNsoArrayBuffer(), nsoArrayBuffer)).to.equal(true);
   });
@@ -211,9 +234,9 @@ describe('Nintendo Switch Online - Gameboy', () => {
 
     const nsoSaveData = NsoGameboySaveData.createFromNsoData(nsoArrayBuffer);
 
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedRomHash(), GBC_ROM_HASH)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getEncodedVersion(), GBC_VERSION_NUMBER)).to.equal(true);
-    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getFileFormat(), GBC_FILE_FORMAT)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRomHashArrayBuffer(), GBC_ROM_HASH)).to.equal(true);
+    expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getGitRevisionNumberArrayBuffer(), GBC_GIT_REVISION_NUMBER)).to.equal(true);
+    expect(nsoSaveData.getRtcDataArrayBuffer()).to.equal(GBC_RTC_DATA);
     expect(ArrayBufferUtil.arrayBuffersEqual(nsoSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 });
