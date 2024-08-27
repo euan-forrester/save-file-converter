cad2fc223c8662dc8ff6738bd60c09eb3e9eb7a9
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2024-08-26 22:48:38 -0700

    Added 512B save

--- frontend/tests/unit/save-formats/OnlineEmulators/Emulators/mGba.spec.js
+++ frontend/tests/unit/save-formats/OnlineEmulators/Emulators/mGba.spec.js
@@ -14,10 +14,8 @@ const EMULATOR_INVALID_SAVE_STATE_FILE = `${DIR}/invalid-save-state.save`;
 // So we have a couple of examples of most of the save types to help make sure we're consistently finding the right
 // offsets in each save state file.
 
-/*
-const EMULATOR_EEPROM_512B_FILENAME = `${DIR}/donkey-kong-country-3.save`;
-const RAW_EEPROM_512B_FILENAME = `${DIR}/donkey-kong-country-3.sav`;
-*/
+const EMULATOR_EEPROM_512B_FILENAME = `${DIR}/baldurs-gate.state`;
+const RAW_EEPROM_512B_FILENAME = `${DIR}/baldurs-gate.sav`;
 
 const EMULATOR_EEPROM_8KB_FILENAME = `${DIR}/zelda-minish-cap.state`;
 const RAW_EEPROM_8KB_FILENAME = `${DIR}/zelda-minish-cap.sav`;
@@ -41,7 +39,6 @@ const EMULATOR_FLASH_RAM_128KB_FILENAME_2 = `${DIR}/super-mario-advance-4.state`
 const RAW_FLASH_RAM_128KB_FILENAME_2 = `${DIR}/super-mario-advance-4.sav`;
 
 describe('OnlineEmulators - GBA - mGBA', () => {
-  /*
   it('should convert an emulator save state containing a 512B EEPROM save to raw format', async () => {
     const emulatorSaveStateArrayBuffer = await ArrayBufferUtil.readArrayBuffer(EMULATOR_EEPROM_512B_FILENAME);
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_EEPROM_512B_FILENAME);
@@ -50,7 +47,6 @@ describe('OnlineEmulators - GBA - mGBA', () => {
 
     expect(ArrayBufferUtil.arrayBuffersEqual(emulatorSaveStateData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
-  */
 
   it('should convert an emulator save state containing a 8kB EEPROM save to raw format', async () => {
     const emulatorSaveStateArrayBuffer = await ArrayBufferUtil.readArrayBuffer(EMULATOR_EEPROM_8KB_FILENAME);
