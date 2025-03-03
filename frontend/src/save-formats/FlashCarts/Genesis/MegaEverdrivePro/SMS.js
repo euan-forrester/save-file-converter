a6391635c4f54db71606ccd4986355d5c5a65dcb
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2025-03-03 14:51:53 -0800

    Added support for converting byte-expanded SMS saves from knockoff Everdrives (#361)

--- frontend/src/save-formats/FlashCarts/Genesis/MegaEverdrivePro/SMS.js
+++ frontend/src/save-formats/FlashCarts/Genesis/MegaEverdrivePro/SMS.js
@@ -1,8 +1,24 @@
 import SaveFilesUtil from '../../../../util/SaveFiles';
+import GenesisUtil from '../../../../util/Genesis';
+
+// Some knockoff everdrives produce byte-expanded files for SMS games even though the regular everdrives don't.
+// So let's just silently unexpand them when converting to raw
+//
+// This doesn't seem to be a major issue (only 1 report in a year) so let's not clutter the interface by offering the option
+// to expand the files when converting to flash cart format. It seems much more likely that someone would be converting their
+// saves *from* a knockoff cart than *to* a knockoff cart
+//
+// If someone does need to convert their saves to a cart that expects them byte-expanded, they can use https://savefileconverter.com/#/utilities/advanced?tab=byte-expansion
 
 export default class GenesisMegaEverdriveProSmsFlashCartSaveData {
   static createFromFlashCartData(flashCartArrayBuffer) {
-    return new GenesisMegaEverdriveProSmsFlashCartSaveData(flashCartArrayBuffer, flashCartArrayBuffer);
+    let rawArrayBuffer = flashCartArrayBuffer;
+
+    if (GenesisUtil.isByteExpanded(flashCartArrayBuffer)) {
+      rawArrayBuffer = GenesisUtil.byteCollapse(flashCartArrayBuffer);
+    }
+
+    return new GenesisMegaEverdriveProSmsFlashCartSaveData(flashCartArrayBuffer, rawArrayBuffer);
   }
 
   static createFromRawData(rawArrayBuffer) {
--- frontend/tests/unit/save-formats/FlashCarts/Genesis/MegaEverdrivePro/SMS.spec.js
+++ frontend/tests/unit/save-formats/FlashCarts/Genesis/MegaEverdrivePro/SMS.spec.js
@@ -7,6 +7,9 @@ const DIR = './tests/data/save-formats/flashcarts/genesis';
 
 const MEGA_EVERDRIVE_PRO_SMS_FILENAME = `${DIR}/megaeverdrivepro/Phantasy Star (USA, Europe) (Rev A).srm`;
 
+const KNOCKOFF_EVERDRIVE_SMS_FILENAME = `${DIR}/megaeverdrivepro/Phantasy_Star_USA_Europe_Rev_A-byte-expanded.srm`;
+const RAW_SMS_FILENAME = `${DIR}/megaeverdrivepro/Phantasy_Star_USA_Europe_Rev_A-byte-collapsed.srm`;
+
 describe('Flash cart - Genesis - Mega Everdrive Pro - SMS', () => {
   it('should convert a raw SMS save to Mega Everdrive Pro format', async () => {
     const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(MEGA_EVERDRIVE_PRO_SMS_FILENAME);
@@ -22,5 +25,14 @@ describe('Flash cart - Genesis - Mega Everdrive Pro - SMS', () => {
     const flashCartSaveData = GenesisMegaEverdriveProSmsFlashCartSaveData.createFromFlashCartData(flashCartArrayBuffer);
 
     expect(ArrayBufferUtil.arrayBuffersEqual(flashCartSaveData.getFlashCartArrayBuffer(), flashCartSaveData.getRawArrayBuffer())).to.equal(true);
+  });
+
+  it('should convert a byte-expanded knockoff Everdrive SMS save to raw format', async () => {
+    const flashCartArrayBuffer = await ArrayBufferUtil.readArrayBuffer(KNOCKOFF_EVERDRIVE_SMS_FILENAME);
+    const rawArrayBuffer = await ArrayBufferUtil.readArrayBuffer(RAW_SMS_FILENAME);
+
+    const flashCartSaveData = GenesisMegaEverdriveProSmsFlashCartSaveData.createFromFlashCartData(flashCartArrayBuffer);
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(flashCartSaveData.getRawArrayBuffer(), rawArrayBuffer)).to.equal(true);
   });
 });
