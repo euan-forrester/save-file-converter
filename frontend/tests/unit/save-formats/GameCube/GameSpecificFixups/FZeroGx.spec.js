b5e63e55a3321603408f8a4ba9632e0ea97312d6
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2025-05-26 13:20:18 -0700

    Add tests to make sure we're actually changing our files when we fix them up

--- frontend/tests/unit/save-formats/GameCube/GameSpecificFixups/FZeroGx.spec.js
+++ frontend/tests/unit/save-formats/GameCube/GameSpecificFixups/FZeroGx.spec.js
@@ -9,6 +9,22 @@ const DIR = './tests/data/save-formats/gamecube/GameSpecificFixups';
 const GCI_FILENAME = `${DIR}/f_zero_gx_usa.gci`;
 
 describe('GameCube - game-specific fixups - F-Zero GX', () => {
+  it('should change an F-Zero GX file when fixing it up', async () => {
+    const gciArrayBuffer = await ArrayBufferUtil.readArrayBuffer(GCI_FILENAME);
+
+    const gameCubeSaveFiles = GameCubeGciSaveData.convertGcisToSaveFiles([gciArrayBuffer]);
+
+    expect(gameCubeSaveFiles.length).to.equal(1);
+
+    const headerSerials = {
+      serial1: 0,
+      serial2: 0,
+    };
+    const fixedSaveFile = FZeroGxFixups.fixupSaveFile(gameCubeSaveFiles[0], headerSerials);
+
+    expect(ArrayBufferUtil.arrayBuffersEqual(gameCubeSaveFiles[0].rawData, fixedSaveFile.rawData)).to.equal(false);
+  });
+
   it('should correctly fixup an F-Zero GX file', async () => {
     const gciArrayBuffer = await ArrayBufferUtil.readArrayBuffer(GCI_FILENAME);
 
