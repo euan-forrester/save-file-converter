1f491405935be9a5ddfcd74b805b2153779d1d19
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2025-05-26 14:30:32 -0700

    Some small fixups for .gci parsing

--- frontend/src/save-formats/GameCube/Gci.js
+++ frontend/src/save-formats/GameCube/Gci.js
@@ -3,16 +3,25 @@
 /*
 The standard format for individual saves on the GameCube appears to be the .GCI format.
 
-It's a directory entry concatenated with the raw save data
+There are other potential formats recognized by Dolphin:
+- .GCS: https://github.com/dolphin-emu/dolphin/blob/master/Source/Core/Core/HW/GCMemcard/GCMemcardUtils.cpp#L149
+- .SAV: https://github.com/dolphin-emu/dolphin/blob/master/Source/Core/Core/HW/GCMemcard/GCMemcardUtils.cpp#L182
+
+Dolphin distinguishes between them based on file size, because the blocks are the same but the header is different: https://github.com/dolphin-emu/dolphin/blob/master/Source/Core/Core/HW/GCMemcard/GCMemcardUtils.cpp#L212
+
+.GCI is a directory entry concatenated with the raw save data
 
 Note that the starting block number in the directory entry is irrelevant here because the save will not be put back where it was:
 https://github.com/suloku/gcmm/blob/95c737c2af0ebecfa2ef02a8c6c30496d0036e87/source/mcard.c#L225
 
 0x00-0x3F: Directory entry
-0x40-????: Game data (including the comment and icons)
+0x40-EOF:  Game data (including the comment and icons)
 */
 
+import GameCubeBasics from './Components/Basics';
 import GameCubeDirectoryEntry from './Components/DirectoryEntry';
+
+const { BLOCK_SIZE } = GameCubeBasics;
 
 const GAME_CODE_AND_FILE_NAME_ENCODING = 'US-ASCII'; // In theory we could parse the whole object with shift-jis because the ASCII stuff will be decoded correctly, with the exception of backslash and tilde and then anything in "extended" ASCII above 0x7F
 
@@ -26,16 +35,22 @@ export default class GameCubeGciSaveData {
     });
   }
 
+  // Based on https://github.com/dolphin-emu/dolphin/blob/master/Source/Core/Core/HW/GCMemcard/GCMemcardUtils.cpp#L131
   static convertGcisToSaveFiles(arrayBuffers, overrideCommentEncoding) {
+    const commentEncoding = (overrideCommentEncoding !== undefined) ? overrideCommentEncoding : GAME_CODE_AND_FILE_NAME_ENCODING;
+
     return arrayBuffers.map((arrayBuffer) => {
       // It does not appear that we can automatically detect the encoding of the comments.
       // The encoding-japanese package that we use to encode/decode shift-jis strings can in theory
       // detect encoding, but for our example save files it returns "UTF32" for US-ASCII comments,
       // and any one of "UTF32", "UTF16", or "SJIS" for the Japanese ones (which are all in shift-jis)
-      const commentEncoding = (overrideCommentEncoding !== undefined) ? overrideCommentEncoding : GAME_CODE_AND_FILE_NAME_ENCODING;
-
       const directoryEntry = GameCubeDirectoryEntry.readDirectoryEntry(arrayBuffer, GAME_CODE_AND_FILE_NAME_ENCODING);
       const rawData = arrayBuffer.slice(DATA_OFFSET);
+
+      if (rawData.byteLength !== (directoryEntry.saveSizeBlocks * BLOCK_SIZE)) {
+        throw new Error(`File appears to be corrupt. Save size specified as ${directoryEntry.saveSizeBlocks} blocks (${directoryEntry.saveSizeBlocks * BLOCK_SIZE} bytes)`
+          + ` but save data is ${rawData.byteLength} bytes`);
+      }
 
       const comments = GameCubeDirectoryEntry.getComments(directoryEntry.commentStart, rawData, commentEncoding);
 
