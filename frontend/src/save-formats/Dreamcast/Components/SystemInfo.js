65fab82de90e139f54adc50303de0314281cd4be
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2025-10-08 16:02:14 -0700

    Fixed up some fields in the system info block after looking at libevmu

--- frontend/src/save-formats/Dreamcast/Components/SystemInfo.js
+++ frontend/src/save-formats/Dreamcast/Components/SystemInfo.js
@@ -4,7 +4,9 @@
 Dreamcast system info block
 
 Format taken from:
+- https://vmu.falcogirgis.net/filesystem.html
 - https://mc.pp.se/dc/vms/flashmem.html
+- https://github.com/gyrovorbis/libevmu/blob/libgimbal-refactor/lib/api/evmu/fs/evmu_fat.h#L120
 - https://segaxtreme.net/resources/maple-bus-1-0-function-type-specifications-ft1-storage-function.195/
 - https://github.com/flyinghead/flycast/blob/33833cfd1ed2d94d907223442fdb8cdafd8d5d80/core/hw/maple/maple_devs.cpp#L510
 
@@ -24,15 +26,17 @@ Format taken from:
 0x48-0x49 : size of FAT in blocks (1)
 0x4a-0x4b : location of directory (253)
 0x4c-0x4d : size of directory in blocks (13)
-0x4e-0x4f : icon shape for this VMS (0-123) (this is described as one byte for volume icon and one byte reserved here: https://github.com/flyinghead/flycast/blob/33833cfd1ed2d94d907223442fdb8cdafd8d5d80/core/hw/maple/maple_devs.cpp#L525)
-0x50-0x51 : number of save blocks (200) (the official docs say that this is the block number of the start of the save area, however that would be 199 and this contains 200)
-0x52-0x53 : unknown value (31): listed in flycast as "number of save blocks" but 31 doesn't match the size or location of the save area, nor the number of empty blocks between the save area and the system blocks. https://github.com/flyinghead/flycast/blob/33833cfd1ed2d94d907223442fdb8cdafd8d5d80/core/hw/maple/maple_devs.cpp#L531. It's listed in the official docs along with the previous value as specifying the save area: start block number and then number of blocks. However, that would be the values 199 and 200 respectively
-0x54-0x57 : reserved (something for execution files): https://github.com/flyinghead/flycast/blob/33833cfd1ed2d94d907223442fdb8cdafd8d5d80/core/hw/maple/maple_devs.cpp#L533. The official docs don't shed much light here either "This is fixed at "0000h" when an execution file cannot be executed from this partition. If an execution file can be executed, refer to the specifications for that peripheral for further details."
+0x4e      : icon shape for this VMS (0-123)
+0x4f      : unknown (sort flag? https://github.com/gyrovorbis/libevmu/blob/libgimbal-refactor/lib/api/evmu/fs/evmu_fat.h#L145 reserved? https://github.com/flyinghead/flycast/blob/33833cfd1ed2d94d907223442fdb8cdafd8d5d80/core/hw/maple/maple_devs.cpp#L525)
+0x50-0x51 : size of the save area (200) (libevum describes it as the location of the extra region, but the other regions are specified by their ending block number not beginning) https://github.com/gyrovorbis/libevmu/blob/libgimbal-refactor/lib/api/evmu/fs/evmu_fat.h#L146 (the official docs say that this is the block number of the start of the save area, however that would be 199 and this contains 200)
+0x52-0x53 : size of the extra area (41) https://github.com/gyrovorbis/libevmu/blob/libgimbal-refactor/lib/api/evmu/fs/evmu_fat.h#L147 listed in flycast as "number of save blocks" and set at 31 which doesn't match the size or location of the save area, nor the number of empty blocks between the save area and the system blocks. https://github.com/flyinghead/flycast/blob/33833cfd1ed2d94d907223442fdb8cdafd8d5d80/core/hw/maple/maple_devs.cpp#L531. It's listed in the official docs along with the previous value as specifying the save area: start block number and then number of blocks. However, that would be the values 199 and 200 respectively
+0x54-0x55 : game block (0) (starting location for game file): https://github.com/gyrovorbis/libevmu/blob/libgimbal-refactor/lib/api/evmu/fs/evmu_fat.h#L148
+0x56-0x57 : game size (128) (maximum size for game file): https://github.com/gyrovorbis/libevmu/blob/libgimbal-refactor/lib/api/evmu/fs/evmu_fat.h#L149
 
 Notes:
 
 0x10-0x2f : the official docs say that this is the volume label "The contents of this field can be anything.".
-            https://mc.pp.se/dc/vms/flashmem.html says it's the VMU color then unused bytes as listed above. The sample file I found in practice conformed to this format.
+            https://vmu.falcogirgis.net/filesystem.html says it's the VMU color then unused bytes as listed above, as does libevmu. The sample file I found in practice conformed to this format.
 */
 
 import Util from '../../../util/util';
@@ -64,13 +68,11 @@ const DIRECTORY_BLOCK_NUMBER_OFFSET = 0x4A;
 const DIRECTORY_SIZE_IN_BLOCKS_OFFSET = 0x4C;
 const ICON_SHAPE_OFFSET = 0x4E;
 const SAVE_AREA_SIZE_IN_BLOCKS_OFFSET = 0x50;
-const UNKNOWN_VALUE_OFFSET = 0x52;
-const RESERVED_OFFSET = 0x54;
+const EXTRA_AREA_SIZE_IN_BLOCKS_OFFSET = 0x52;
+const GAME_BLOCK_OFFSET = 0x54;
+const MAX_GAME_SIZE_OFFSET = 0x56;
 
 const DEFAULT_PARTITION_NUMBER = 0;
-const DEFAULT_RESERVED_VALUE = 0;
-
-const UNKNOWN_VALUE = 31; // See above at offset 0x52
 
 const PADDING_VALUE = 0x00;
 
@@ -110,16 +112,13 @@ export default class DreamcastSystemInfo {
     dataView.setUint16(DIRECTORY_BLOCK_NUMBER_OFFSET, DreamcastBasics.DIRECTORY_BLOCK_NUMBER, LITTLE_ENDIAN);
     dataView.setUint16(DIRECTORY_SIZE_IN_BLOCKS_OFFSET, DreamcastBasics.DIRECTORY_SIZE_IN_BLOCKS, LITTLE_ENDIAN);
 
-    dataView.setUint16(ICON_SHAPE_OFFSET, volumeInfo.iconShape, LITTLE_ENDIAN);
+    dataView.setUint8(ICON_SHAPE_OFFSET, volumeInfo.iconShape);
 
     dataView.setUint16(SAVE_AREA_SIZE_IN_BLOCKS_OFFSET, DreamcastBasics.SAVE_AREA_SIZE_IN_BLOCKS, LITTLE_ENDIAN);
-    dataView.setUint16(UNKNOWN_VALUE_OFFSET, UNKNOWN_VALUE, LITTLE_ENDIAN);
+    dataView.setUint16(EXTRA_AREA_SIZE_IN_BLOCKS_OFFSET, DreamcastBasics.EXTRA_AREA_SIZE_IN_BLOCKS, LITTLE_ENDIAN);
 
-    if (Object.hasOwn(volumeInfo, 'reserved')) {
-      dataView.setUint32(RESERVED_OFFSET, volumeInfo.reserved, LITTLE_ENDIAN);
-    } else {
-      dataView.setUint32(RESERVED_OFFSET, DEFAULT_RESERVED_VALUE, LITTLE_ENDIAN);
-    }
+    dataView.setUint16(GAME_BLOCK_OFFSET, DreamcastBasics.DEFAULT_GAME_BLOCK, LITTLE_ENDIAN);
+    dataView.setUint16(MAX_GAME_SIZE_OFFSET, DreamcastBasics.DEFAULT_MAX_GAME_SIZE, LITTLE_ENDIAN);
 
     return arrayBuffer;
   }
@@ -149,11 +148,13 @@ export default class DreamcastSystemInfo {
     const directoryBlockNumber = dataView.getUint16(DIRECTORY_BLOCK_NUMBER_OFFSET, LITTLE_ENDIAN);
     const directorySizeInBlocks = dataView.getUint16(DIRECTORY_SIZE_IN_BLOCKS_OFFSET, LITTLE_ENDIAN);
 
-    const iconShape = dataView.getUint16(ICON_SHAPE_OFFSET, LITTLE_ENDIAN);
+    const iconShape = dataView.getUint8(ICON_SHAPE_OFFSET);
 
     const saveAreaSizeInBlocks = dataView.getUint16(SAVE_AREA_SIZE_IN_BLOCKS_OFFSET, LITTLE_ENDIAN);
+    const extraAreaSizeInBlocks = dataView.getUint16(EXTRA_AREA_SIZE_IN_BLOCKS_OFFSET, LITTLE_ENDIAN);
 
-    const reserved = dataView.getUint32(RESERVED_OFFSET, LITTLE_ENDIAN);
+    const gameBlock = dataView.getUint16(GAME_BLOCK_OFFSET, LITTLE_ENDIAN);
+    const maxGameSize = dataView.getUint16(MAX_GAME_SIZE_OFFSET, LITTLE_ENDIAN);
 
     return {
       useCustomColor,
@@ -174,11 +175,16 @@ export default class DreamcastSystemInfo {
         sizeInBlocks: directorySizeInBlocks,
       },
       iconShape,
+      extraArea: {
+        blockNumber: DreamcastBasics.EXTRA_AREA_BLOCK_NUMBER,
+        sizeInBlocks: extraAreaSizeInBlocks,
+      },
       saveArea: {
         blockNumber: DreamcastBasics.SAVE_AREA_BLOCK_NUMBER,
         sizeInBlocks: saveAreaSizeInBlocks,
       },
-      reserved,
+      gameBlock,
+      maxGameSize,
     };
   }
 }
