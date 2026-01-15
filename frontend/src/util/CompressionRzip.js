5e11d392f9bf6c89963329a3ca9da491fe4d38f5
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2026-01-15 15:54:46 -0800

    Imported rzipjs and added tests

---
+++ frontend/src/util/CompressionRzip.js
@@ -0,0 +1,35 @@
+import { RzipJS } from 'RzipJS';
+
+import Util from './util';
+
+export default class CompressionRzip {
+  static decompress(arrayBuffer) {
+    try {
+      const rzip = new RzipJS(new Uint8Array(arrayBuffer));
+
+      if (!rzip.is_rzip_compressed()) {
+        throw new Error('Data is not compressed with rzip');
+      }
+
+      return Util.bufferToArrayBuffer(rzip.rzip_inflate());
+    } catch (e) {
+      console.log(e);
+      throw new Error('Could not decompress the data using rzip', e);
+    }
+  }
+
+  static compress(arrayBuffer) {
+    try {
+      const rzip = new RzipJS(new Uint8Array(arrayBuffer));
+
+      if (rzip.is_rzip_compressed()) {
+        throw new Error('Data is already compressed using rzip');
+      }
+
+      return Util.bufferToArrayBuffer(rzip.rzip_deflate());
+    } catch (e) {
+      console.log(e);
+      throw new Error('Could not compress the data using rzip', e);
+    }
+  }
+}
